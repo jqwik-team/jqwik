e88c3615587f46a5166623f2c167ccd9ea2c21d6
Johannes Link <business@johanneslink.net>
2017-03-20 21:36:42 +0100

    got rid of PotentialContainerDiscoverySpec

--- src/main/java/net/jqwik/discovery/specs/GroupDiscoverySpec.java
+++ src/main/java/net/jqwik/discovery/specs/GroupDiscoverySpec.java
@@ -1,24 +1,25 @@
 package net.jqwik.discovery.specs;
 
 import net.jqwik.api.Group;
+import net.jqwik.discovery.predicates.IsPotentialTestContainer;
 import net.jqwik.discovery.predicates.IsTopLevelClass;
 
 import java.util.function.Predicate;
 
 import static net.jqwik.support.JqwikReflectionSupport.isStatic;
 
-public class GroupDiscoverySpec extends PotentialContainerDiscoverySpec {
+public class GroupDiscoverySpec implements DiscoverySpec<Class<?>> {
 
+	private final static Predicate<Class<?>> isPotentialTestContainer = new IsPotentialTestContainer();
 	private final static Predicate<Class<?>> isTopLevelClass = new IsTopLevelClass();
 	private final static Predicate<Class<?>> hasGroupAnnotation = candidate -> candidate.isAnnotationPresent(Group.class);
 
 	@Override
 	public boolean shouldBeDiscovered(Class<?> candidate) {
-		if (!super.shouldBeDiscovered(candidate))
-			return false;
-		if (isTopLevelClass.test(candidate))
-			return false;
-		return hasGroupAnnotation.test(candidate);
+		return isPotentialTestContainer
+				.and(isTopLevelClass.negate())
+				.and(hasGroupAnnotation)
+				.test(candidate);
 	}
 
 	@Override
--- src/main/java/net/jqwik/discovery/specs/TopLevelContainerDiscoverySpec.java
+++ src/main/java/net/jqwik/discovery/specs/TopLevelContainerDiscoverySpec.java
@@ -1,31 +1,33 @@
 package net.jqwik.discovery.specs;
 
 import net.jqwik.api.Group;
+import net.jqwik.discovery.predicates.IsPotentialTestContainer;
 import net.jqwik.discovery.predicates.IsTopLevelClass;
 
 import java.util.function.Predicate;
 
 import static net.jqwik.support.JqwikReflectionSupport.isStatic;
 
-public class TopLevelContainerDiscoverySpec extends PotentialContainerDiscoverySpec {
+public class TopLevelContainerDiscoverySpec implements DiscoverySpec<Class<?>> {
 
+	private final static Predicate<Class<?>> isPotentialTestContainer = new IsPotentialTestContainer();
 	private final static Predicate<Class<?>> isTopLevelClass = new IsTopLevelClass();
 	private final static Predicate<Class<?>> isStaticNonGroupMember = candidate -> isStatic(candidate) && !candidate.isAnnotationPresent(Group.class);
 
 	@Override
 	public boolean shouldBeDiscovered(Class<?> candidate) {
-		if (!super.shouldBeDiscovered(candidate))
-			return false;
-		return isTopLevelClass.test(candidate) || isStaticNonGroupMember.test(candidate);
+		return isPotentialTestContainer
+				.and(isTopLevelClass.or(isStaticNonGroupMember))
+				.test(candidate);
 	}
 
 	@Override
 	public boolean butSkippedOnExecution(Class<?> candidate) {
-		return super.butSkippedOnExecution(candidate);
+		return false;
 	}
 
 	@Override
 	public String skippingReason(Class<?> candidate) {
-		return super.skippingReason(candidate);
+		return null;
 	}
 }
