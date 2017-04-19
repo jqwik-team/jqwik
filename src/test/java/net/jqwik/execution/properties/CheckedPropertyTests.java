cf7ee9314b265a14ab364353076e84377941e08f
Johannes Link <business@johanneslink.net>
2017-04-19 17:23:49 +0200

    Refactoring CheckedPropertyTests

--- src/test/java/net/jqwik/execution/properties/CheckedPropertyTests.java
+++ src/test/java/net/jqwik/execution/properties/CheckedPropertyTests.java
@@ -6,7 +6,7 @@ import net.jqwik.api.properties.*;
 import net.jqwik.descriptor.*;
 import net.jqwik.properties.*;
 import net.jqwik.properties.arbitraries.*;
-import org.junit.platform.engine.*;
+import org.opentest4j.*;
 
 import java.lang.reflect.*;
 import java.util.*;
@@ -14,7 +14,6 @@ import java.util.*;
 import static net.jqwik.execution.properties.TestHelper.*;
 import static net.jqwik.properties.PropertyCheckResult.Status.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.platform.engine.TestExecutionResult.Status.*;
 
 class CheckedPropertyTests {
 
@@ -23,7 +22,7 @@ class CheckedPropertyTests {
 		@Example
 		void createCheckedPropertyWithoutTriesParameter() throws NoSuchMethodException {
 			PropertyMethodDescriptor descriptor = (PropertyMethodDescriptor) TestDescriptorBuilder
-				.forMethod(Examples.class, "propertyWithoutTries", int.class).build();
+				.forMethod(BooleanReturningExamples.class, "propertyWithoutTries", int.class).build();
 			CheckedPropertyFactory factory = new CheckedPropertyFactory();
 			CheckedProperty checkedProperty = factory.fromDescriptor(descriptor, new Object());
 
@@ -33,7 +32,7 @@ class CheckedPropertyTests {
 		@Example
 		void createCheckedPropertyWithTriesParameter() throws NoSuchMethodException {
 			PropertyMethodDescriptor descriptor = (PropertyMethodDescriptor) TestDescriptorBuilder
-				.forMethod(Examples.class, "propertyWith42Tries", int.class).build();
+				.forMethod(BooleanReturningExamples.class, "propertyWith42Tries", int.class).build();
 			CheckedPropertyFactory factory = new CheckedPropertyFactory();
 			CheckedProperty checkedProperty = factory.fromDescriptor(descriptor, new Object());
 
@@ -41,55 +40,74 @@ class CheckedPropertyTests {
 		}
 	}
 
-	@Example
-	void intParametersSuccess() {
-		intOnlyExample("prop1", params -> params.size() == 1, SATISFIED);
-		intOnlyExample("prop2", params -> params.size() == 2, SATISFIED);
-		intOnlyExample("prop3", params -> params.size() == 3, SATISFIED);
-		intOnlyExample("prop4", params -> params.size() == 4, SATISFIED);
-		intOnlyExample("prop5", params -> params.size() == 5, SATISFIED);
-		intOnlyExample("prop6", params -> params.size() == 6, SATISFIED);
-		intOnlyExample("prop7", params -> params.size() == 7, SATISFIED);
-		intOnlyExample("prop8", params -> params.size() == 8, SATISFIED);
+	@Group
+	class PropertiesReturningBoolean {
+		@Example
+		void intParametersSuccess() {
+			intOnlyExample("prop0", params -> params.size() == 0, SATISFIED);
+			intOnlyExample("prop1", params -> params.size() == 1, SATISFIED);
+			intOnlyExample("prop2", params -> params.size() == 2, SATISFIED);
+			intOnlyExample("prop8", params -> params.size() == 8, SATISFIED);
+		}
+
+		@Example
+		void intParametersFailure() {
+			intOnlyExample("prop0", params -> false, FALSIFIED);
+			intOnlyExample("prop1", params -> false, FALSIFIED);
+			intOnlyExample("prop2", params -> false, FALSIFIED);
+			intOnlyExample("prop8", params -> false, FALSIFIED);
+		}
+
+		@Example
+		void exceptionDuringCheck() {
+			RuntimeException toThrow = new RuntimeException("test");
+			intOnlyExample("prop0", params -> {
+				throw toThrow;
+			}, ERRONEOUS);
+			intOnlyExample("prop8", params -> {
+				throw toThrow;
+			}, ERRONEOUS);
+		}
+
+		@Example
+		void testAbortMakesCheckExhausted() {
+			RuntimeException toThrow = new TestAbortedException("test");
+			intOnlyExample("prop0", params -> {
+				throw toThrow;
+			}, EXHAUSTED);
+			intOnlyExample("prop8", params -> {
+				throw toThrow;
+			}, EXHAUSTED);
+		}
+
+		@Example
+		void ifNoArbitraryForParameterCanBeFound_checkIsErroneous() {
+			List<Parameter> parameters = getParametersForMethod("stringProp");
+			CheckedProperty checkedProperty = new CheckedProperty("stringProp", params -> false,
+				parameters, p -> Optional.empty(), 100, 1000L);
+
+			PropertyCheckResult check = checkedProperty.check();
+			assertThat(check.status()).isEqualTo(PropertyCheckResult.Status.ERRONEOUS);
+			CannotFindArbitraryException cannotFindeArbitraryException = (CannotFindArbitraryException) check.throwable().get();
+			assertThat(cannotFindeArbitraryException.getParameter()).isSameAs(parameters.get(0));
+		}
+
+		@Example
+		void usingASeedWillAlwaysProvideSameArbitraryValues() {
+			List<Integer> allGeneratedInts = new ArrayList<>();
+			CheckedFunction addIntToList = params -> allGeneratedInts.add((int) params.get(0));
+			CheckedProperty checkedProperty = new CheckedProperty("prop1", addIntToList, getParametersForMethod("prop1"),
+				p -> Optional.of(new GenericArbitrary(Arbitraries.integer(-100, 100))), 10, 42L);
+
+			PropertyCheckResult check = checkedProperty.check();
+			assertThat(check.randomSeed()).isEqualTo(42L);
+
+			assertThat(check.status()).isEqualTo(SATISFIED);
+			assertThat(allGeneratedInts).containsExactly(-59, 20, -10, 1, -88, -87, 100, 40, 96, 82);
+		}
+
 	}
 
-	@Example
-	void intParametersFailure() {
-		intOnlyExample("prop1", params -> false, FALSIFIED);
-		intOnlyExample("prop2", params -> false, FALSIFIED);
-		intOnlyExample("prop3", params -> false, FALSIFIED);
-		intOnlyExample("prop4", params -> false, FALSIFIED);
-		intOnlyExample("prop5", params -> false, FALSIFIED);
-		intOnlyExample("prop6", params -> false, FALSIFIED);
-		intOnlyExample("prop7", params -> false, FALSIFIED);
-		intOnlyExample("prop8", params -> false, FALSIFIED);
-	}
-
-	@Example
-	void abortIfNoArbitraryForParameterCanBeFound() {
-		List<Parameter> parameters = getParametersForMethod("stringProp");
-		CheckedProperty checkedProperty = new CheckedProperty("stringProp", params -> false,
-															  parameters, p -> Optional.empty(), 100, 1000L);
-
-		PropertyCheckResult check = checkedProperty.check();
-		assertThat(check.status()).isEqualTo(PropertyCheckResult.Status.ERRONEOUS);
-		CannotFindArbitraryException cannotFindeArbitraryException = (CannotFindArbitraryException) check.throwable().get();
-		assertThat(cannotFindeArbitraryException.getParameter()).isSameAs(parameters.get(0));
-	}
-
-	@Example
-	void usingASeedWillAlwaysProvideSameArbitraryValues() {
-		List<Integer> allGeneratedInts = new ArrayList<>();
-		CheckedFunction addIntToList = params -> allGeneratedInts.add((int) params.get(0));
-		CheckedProperty checkedProperty = new CheckedProperty("prop1", addIntToList, getParametersForMethod("prop1"),
-			p -> Optional.of(new GenericArbitrary(Arbitraries.integer(-100, 100))), 10, 42L);
-
-		PropertyCheckResult check = checkedProperty.check();
-		assertThat(check.randomSeed()).isEqualTo(42L);
-
-		assertThat(check.status()).isEqualTo(SATISFIED);
-		assertThat(allGeneratedInts).containsExactly(-59, 20, -10, 1, -88, -87, 100, 40, 96, 82);
-	}
 
 	private void intOnlyExample(String methodName, CheckedFunction forAllFunction, PropertyCheckResult.Status expectedStatus) {
 		CheckedProperty checkedProperty = new CheckedProperty(methodName, forAllFunction, getParametersForMethod(methodName),
@@ -99,10 +117,10 @@ class CheckedPropertyTests {
 	}
 
 	private List<Parameter> getParametersForMethod(String methodName) {
-		return getParametersFor(Examples.class, methodName);
+		return getParametersFor(BooleanReturningExamples.class, methodName);
 	}
 
-	private static class Examples {
+	private static class BooleanReturningExamples {
 
 		@Property
 		public boolean propertyWithoutTries(@ForAll int anyNumber) {
@@ -118,32 +136,15 @@ class CheckedPropertyTests {
 			return true;
 		}
 
+		public boolean prop0() {
+			return true;
+		}
+
 		public boolean prop1(@ForAll int n1) {
 			return true;
 		}
 
 		public boolean prop2(@ForAll int n1, @ForAll int n2) {
-			return true;
-		}
-
-		public boolean prop3(@ForAll int n1, @ForAll int n2, @ForAll int n3) {
-			return true;
-		}
-
-		public boolean prop4(@ForAll int n1, @ForAll int n2, @ForAll int n3, @ForAll int n4) {
-			return true;
-		}
-
-		public boolean prop5(@ForAll int n1, @ForAll int n2, @ForAll int n3, @ForAll int n4, @ForAll int n5) {
-			return true;
-		}
-
-		public boolean prop6(@ForAll int n1, @ForAll int n2, @ForAll int n3, @ForAll int n4, @ForAll int n5, @ForAll int n6) {
-			return true;
-		}
-
-		public boolean prop7(@ForAll int n1, @ForAll int n2, @ForAll int n3, @ForAll int n4, @ForAll int n5, @ForAll int n6,
-							 @ForAll int n7) {
 			return true;
 		}
 
