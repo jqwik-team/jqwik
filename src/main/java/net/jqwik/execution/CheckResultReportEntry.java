78667ac503d06c355b7ee65edd79c14301cbc29d
Johannes Link <business@johanneslink.net>
2017-05-14 12:43:46 +0200

    sample is no longer reported if it has no elements

--- src/main/java/net/jqwik/execution/CheckResultReportEntry.java
+++ src/main/java/net/jqwik/execution/CheckResultReportEntry.java
@@ -1,23 +1,29 @@
 package net.jqwik.execution;
 
-import net.jqwik.properties.*;
+import java.util.*;
+
 import org.junit.platform.engine.reporting.*;
 
-import java.util.*;
+import net.jqwik.properties.*;
 
 public class CheckResultReportEntry {
 
 	public static final String SEED_REPORT_KEY = "seed";
 	public static final String TRIES_REPORT_KEY = "tries";
 	public static final String CHECKS_REPORT_KEY = "checks";
+	public static final String SAMPLE_REPORT_KEY = "sample";
+	public static final String THROWABLE_REPORT_KEY = "throwable";
 
 	public static ReportEntry from(PropertyCheckResult checkResult) {
 		Map<String, String> entries = new HashMap<>();
 		entries.put(SEED_REPORT_KEY, Long.toString(checkResult.randomSeed()));
 		entries.put(TRIES_REPORT_KEY, Integer.toString(checkResult.countTries()));
 		entries.put(CHECKS_REPORT_KEY, Integer.toString(checkResult.countChecks()));
-		checkResult.sample().ifPresent(sample -> entries.put("sample", sample.toString()));
-		checkResult.throwable().ifPresent(throwable -> entries.put("throwable", throwable.toString()));
+		checkResult.sample().ifPresent(sample -> {
+			if (!sample.isEmpty())
+				entries.put(SAMPLE_REPORT_KEY, sample.toString());
+		});
+		checkResult.throwable().ifPresent(throwable -> entries.put(THROWABLE_REPORT_KEY, throwable.toString()));
 		return ReportEntry.from(entries);
 	}
 
