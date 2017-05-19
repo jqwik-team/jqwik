4ce032994f0dbcb9d9d7a25ff4105f28b6ed8def
Johannes Link <business@johanneslink.net>
2017-05-19 18:12:17 +0200

    Removed duplication between NShrinkableInteger and NShrinkableLong

--- src/main/java/net/jqwik/newArbitraries/NShrinkableInteger.java
+++ src/main/java/net/jqwik/newArbitraries/NShrinkableInteger.java
@@ -1,65 +1,21 @@
 package net.jqwik.newArbitraries;
 
-import net.jqwik.properties.shrinking.*;
-
-import java.util.*;
-import java.util.function.*;
-
-public class NShrinkableInteger implements NShrinkable<Integer> {
-	private final int value;
-	private final int shrinkingTarget;
-
+public class NShrinkableInteger extends NShrinkableIntegral<Integer> {
 	public NShrinkableInteger(int value, int min, int max) {
-		this(value, determineTarget(value, min, max));
+		super(value, min, max);
 	}
 
 	private NShrinkableInteger(int value, int shrinkingTarget) {
-		this.value = value;
-		this.shrinkingTarget = shrinkingTarget;
-	}
-
-	@Override
-	public Set<NShrinkable<Integer>> shrink() {
-		if (shrinkingTarget == value) return Collections.emptySet();
-		return Collections.singleton(shrinkTowards(value, shrinkingTarget));
-	}
-
-	@Override
-	public boolean falsifies(Predicate<Integer> falsifier) {
-		return falsifier.negate().test(value);
+		super(value, shrinkingTarget);
 	}
 
 	@Override
 	public Integer value() {
-		return value;
+		return Math.toIntExact(value);
 	}
 
-	@Override
-	public int distance() {
-		return Math.abs(shrinkingTarget - value);
-	}
-
-	private static int determineTarget(int value, int min, int max) {
-		Range<Integer> range = Range.of(min, max);
-		if (!range.includes(value)) {
-			return value;
-		}
-		if (range.includes(0)) return 0;
-		else {
-			if (value < 0) return max;
-			if (value > 0) return min;
-		}
-		return value; // Should never get here
-	}
-
-	private static NShrinkable<Integer> shrinkTowards(int value, int target) {
-		int next = value - calculateDelta(target, value);
-		return new NShrinkableInteger(next, target);
-	}
-
-	private static int calculateDelta(int current, int target) {
-		if (target > current) return (int) Math.max(Math.floor((target - current) / 2.0), 1);
-		else return (int) Math.min(Math.ceil((target - current) / 2.0), -1);
+	protected NShrinkable<Integer> shrinkTowardsTarget() {
+		return new NShrinkableInteger(Math.toIntExact(nextShrinkValue(value, shrinkingTarget)), Math.toIntExact(shrinkingTarget));
 	}
 
 }
--- src/main/java/net/jqwik/newArbitraries/NShrinkableLong.java
+++ src/main/java/net/jqwik/newArbitraries/NShrinkableLong.java
@@ -1,35 +1,13 @@
 package net.jqwik.newArbitraries;
 
-import net.jqwik.properties.shrinking.*;
-
-import java.util.*;
-import java.util.function.*;
-
-/**
- * TODO: Merge with NShrinkableInteger which is basically identically except of distance() which always returns an int
- */
-public class NShrinkableLong implements NShrinkable<Long> {
-	private final long value;
-	private final long shrinkingTarget;
+public class NShrinkableLong extends NShrinkableIntegral<Long> {
 
 	public NShrinkableLong(long value, long min, long max) {
-		this(value, determineTarget(value, min, max));
+		super(value, min, max);
 	}
 
-	private NShrinkableLong(Long value, Long shrinkingTarget) {
-		this.value = value;
-		this.shrinkingTarget = shrinkingTarget;
-	}
-
-	@Override
-	public Set<NShrinkable<Long>> shrink() {
-		if (shrinkingTarget == value) return Collections.emptySet();
-		return Collections.singleton(shrinkTowards(value, shrinkingTarget));
-	}
-
-	@Override
-	public boolean falsifies(Predicate<Long> falsifier) {
-		return falsifier.negate().test(value);
+	private NShrinkableLong(long value, long shrinkingTarget) {
+		super(value, shrinkingTarget);
 	}
 
 	@Override
@@ -37,35 +15,8 @@ public class NShrinkableLong implements NShrinkable<Long> {
 		return value;
 	}
 
-	@Override
-	public int distance() {
-		long diff = Math.abs(shrinkingTarget - value);
-		if (diff < Integer.MAX_VALUE)
-			return (int) diff;
-		return Integer.MAX_VALUE;
-	}
-
-	private static long determineTarget(long value, long min, long max) {
-		Range<Long> range = Range.of(min, max);
-		if (!range.includes(value)) {
-			return value;
-		}
-		if (range.includes(0L)) return 0;
-		else {
-			if (value < 0) return max;
-			if (value > 0) return min;
-		}
-		return value; // Should never get here
-	}
-
-	private static NShrinkable<Long> shrinkTowards(long value, long target) {
-		long next = value - calculateDelta(target, value);
-		return new NShrinkableLong(next, target);
-	}
-
-	private static long calculateDelta(long current, long target) {
-		if (target > current) return (int) Math.max(Math.floor((target - current) / 2.0), 1);
-		else return (int) Math.min(Math.ceil((target - current) / 2.0), -1);
+	protected NShrinkable<Long> shrinkTowardsTarget() {
+		return new NShrinkableLong(nextShrinkValue(value, shrinkingTarget), shrinkingTarget);
 	}
 
 }
