5be6ed6cb9a7b3bec687fda2bd06054030b3747d
Johannes Link <business@johanneslink.net>
2017-05-28 11:50:09 +0200

    Fixed bug in ParameterListShrinker

--- src/main/java/net/jqwik/properties/ParameterListShrinker.java
+++ src/main/java/net/jqwik/properties/ParameterListShrinker.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.function.*;
 import java.util.stream.*;
 
+// TODO: Split class in two: shrink() and shrinkNext() do not belong together
 public class ParameterListShrinker<T> {
 
 	private final List<Shrinkable<T>> parametersToShrink;
@@ -16,17 +17,17 @@ public class ParameterListShrinker<T> {
 		ArrayList<Shrinkable<T>> resultShrinkables = new ArrayList<>(parametersToShrink);
 
 		Throwable[] lastFalsifiedError = new Throwable[] { originalError };
-		for (int position = 0; position < parametersToShrink.size(); position++) {
-			ShrinkResult<Shrinkable<T>> shrunkPositionResult = shrinkPosition(position, forAllFalsifier);
+		for (int position = 0; position < resultShrinkables.size(); position++) {
+			ShrinkResult<Shrinkable<T>> shrunkPositionResult = shrinkPosition(position, resultShrinkables, forAllFalsifier);
 			shrunkPositionResult.throwable().ifPresent(throwable -> lastFalsifiedError[0] = throwable);
 			resultShrinkables.set(position, shrunkPositionResult.shrunkValue());
 		}
 		return ShrinkResult.of(resultShrinkables, lastFalsifiedError[0]);
 	}
 
-	private ShrinkResult<Shrinkable<T>> shrinkPosition(int position, Predicate<List<T>> forAllFalsifier) {
-		Shrinkable<T> currentShrinkable = parametersToShrink.get(position);
-		Predicate<T> elementFalsifier = createFalsifierForPosition(position, forAllFalsifier);
+	private ShrinkResult<Shrinkable<T>> shrinkPosition(int position, ArrayList<Shrinkable<T>> shrinkables, Predicate<List<T>> forAllFalsifier) {
+		Shrinkable<T> currentShrinkable = shrinkables.get(position);
+		Predicate<T> elementFalsifier = createFalsifierForPosition(position, shrinkables, forAllFalsifier);
 		ValueShrinker<T> shrinker = new ValueShrinker<>(currentShrinkable);
 		return shrinker.shrink(elementFalsifier, null);
 	}
@@ -42,7 +43,7 @@ public class ParameterListShrinker<T> {
 
 	private Set<ShrinkResult<List<Shrinkable<T>>>> shrinkPositionNext(int position, Predicate<List<T>> forAllFalsifier) {
 		Shrinkable<T> currentShrinkable = parametersToShrink.get(position);
-		Predicate<T> elementFalsifier = createFalsifierForPosition(position, forAllFalsifier);
+		Predicate<T> elementFalsifier = createFalsifierForPosition(position, parametersToShrink, forAllFalsifier);
 		Set<ShrinkResult<Shrinkable<T>>> shrinkParameterResults = currentShrinkable.shrinkNext(elementFalsifier);
 		return shrinkParameterResults.stream() //
 				.map(shrinkParameterResult -> shrinkParameterResult //
@@ -54,9 +55,9 @@ public class ParameterListShrinker<T> {
 				.collect(Collectors.toSet());
 	}
 
-	private Predicate<T> createFalsifierForPosition(int position, Predicate<List<T>> forAllFalsifier) {
+	private Predicate<T> createFalsifierForPosition(int position, List<Shrinkable<T>> shrinkables, Predicate<List<T>> forAllFalsifier) {
 		return param -> {
-			List<T> effectiveParams = parametersToShrink.stream().map(Shrinkable::value).collect(Collectors.toList());
+			List<T> effectiveParams = shrinkables.stream().map(Shrinkable::value).collect(Collectors.toList());
 			effectiveParams.set(position, param);
 			return forAllFalsifier.test(effectiveParams);
 		};
