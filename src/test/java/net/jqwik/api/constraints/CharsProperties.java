44db57d48fc028cc517bba3fc41a0c504d06150a
Johannes Link <business@johanneslink.net>
2018-02-19 15:32:28 +0100

    Added tests for Chars constraints

---
+++ src/test/java/net/jqwik/api/constraints/CharsProperties.java
@@ -0,0 +1,23 @@
+package net.jqwik.api.constraints;
+
+import net.jqwik.api.*;
+
+class CharsProperties {
+
+	@Property
+	boolean fromTo(@ForAll @Chars(from = 'a', to = 'z') String aString) {
+		return aString.chars().allMatch(aChar -> aChar >= 'a' && aChar <= 'z');
+	}
+
+	@Property
+	boolean charArray(@ForAll @Chars({ '1', '2', '3' }) String aString) {
+		return aString.chars().allMatch(aChar -> aChar >= '1' && aChar <= '3');
+	}
+
+	@Property
+	boolean listOfChars(@ForAll @Chars({ '1', '2', '3' }) @Chars(from = 'a', to = 'z') String aString) {
+		return aString.chars().allMatch(aChar -> //
+		(aChar >= '1' && aChar <= '3') //
+				|| (aChar >= 'a' && aChar <= 'z'));
+	}
+}
