1fb691695ee2aedb8e57ef9fd2661ce2094cb1c2
Johannes Link <business@johanneslink.net>
2018-02-22 08:06:47 +0100

    Made Tuple4 class public.

--- src/main/java/net/jqwik/api/Tuples.java
+++ src/main/java/net/jqwik/api/Tuples.java
@@ -101,7 +101,7 @@ public class Tuples {
 		}
 	}
 
-	static class Tuple4<T1, T2, T3, T4> extends Tuple3<T1, T2, T3> {
+	public static class Tuple4<T1, T2, T3, T4> extends Tuple3<T1, T2, T3> {
 		protected final T4 v4;
 
 		private Tuple4(T1 v1, T2 v2, T3 v3, T4 v4) {
