71411e1d3b1b637c8b922b84f9d47a01c8ec47dd
Johannes Link <business@johanneslink.net>
2018-03-01 14:07:11 +0100

    Added example from issue https://github.com/jlink/jqwik/issues/10

---
+++ src/test/java/examples/packageWithProperties/NumberCoverageExamples.java
@@ -0,0 +1,32 @@
+package examples.packageWithProperties;
+
+import net.jqwik.api.*;
+import org.assertj.core.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberCoverageExamples {
+
+
+	public static final Condition<Long> EVEN = new Condition<>(x -> x % 2 == 0, "even");
+	public static final Condition<Long> SMALL = new Condition<>(x -> x < 2000, "< 2000");
+
+	@Provide
+	Arbitrary<Long> evenNumbers() {
+		return Arbitraries.longs().filter(l -> l % 2 == 0);
+	}
+
+	@Property
+	void evenNumbersAreEven(@ForAll("evenNumbers") long evenNumber) {
+		assertThat(evenNumber)
+			.is(EVEN);
+	}
+
+	@Property
+	void evenNumbersAreEvenAndSmall(@ForAll("evenNumbers") long evenNumber) {
+		//System.out.println(evenNumber);
+		assertThat(evenNumber)
+			.is(EVEN)
+			.is(SMALL);
+	}
+}
