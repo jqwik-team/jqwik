e3878f00fc8d3ad71b29ac3e145359f5b6973bdc
Johannes Link <business@johanneslink.net>
2018-04-21 15:06:53 +0200

    Splitting shrinking examples in two classes

--- src/test/java/examples/docs/ShrinkingExamples.java
+++ src/test/java/examples/docs/ShrinkingExamples.java
@@ -14,44 +14,6 @@ class ShrinkingExamples {
 		return aString.length() > 5 || aString.length() < 2;
 	}
 
-	@Property(seed="2", reporting = Reporting.GENERATED)
-	boolean shrinkingCanTakeLong(@ForAll("first") String first, @ForAll("second") String second) {
-		String aString = first + second;
-		return aString.length() > 5 || aString.length() < 4;
-	}
-
-	@Provide
-	Arbitrary<String> first() {
-		return Arbitraries.strings() //
-				.withCharRange('a', 'z') //
-				.ofMinLength(1) //
-				.ofMaxLength(10) //
-				.filter(string -> string.endsWith("h"));
-	}
-
-	@Provide
-	Arbitrary<String> second() {
-		return Arbitraries.strings().withCharRange('0', '9').ofMinLength(0).ofMaxLength(10).filter(string -> string.length() >= 1);
-	}
-
-	@Property(reporting = Reporting.FALSIFIED)
-	boolean shouldShrinkToAAH(@ForAll("aVariableString") String aString) {
-		return aString.length() > 3 || aString.length() < 2;
-	}
-
-	@Provide
-	Arbitrary<String> aVariableString() {
-		return Arbitraries.strings() //
-						  .withCharRange('a', 'z') //
-						  .ofMinLength(1) //
-						  .ofMaxLength(10) //
-						  .filter(string -> string.endsWith("h"));
-	}
-
-	@Property(shrinking = ShrinkingMode.OFF)
-	void aPropertyWithLongShrinkingTimes(@ForAll List<Set<String>> list1, @ForAll List<Set<String>> list2) {
-	}
-
 	@Property(shrinking = ShrinkingMode.BOUNDED, reporting = Reporting.FALSIFIED)
 	// Should shrink to 46341 - the smallest number whose square is bigger than Integer.MAX_VALUE
 	boolean rootOfSquareShouldBeOriginalValue(@Positive @ForAll int anInt) {
---
+++ src/test/java/examples/docs/ShrinkingFilterExamples.java
@@ -0,0 +1,46 @@
+package examples.docs;
+
+import net.jqwik.api.*;
+
+class ShrinkingFilterExamples {
+
+	@Property(seed = "2", reporting = Reporting.GENERATED)
+	boolean shrinkingCanTakeLong(@ForAll("first") String first, @ForAll("second") String second) {
+		String aString = first + second;
+		return aString.length() > 5 || aString.length() < 4;
+	}
+
+	@Provide
+	Arbitrary<String> first() {
+		return Arbitraries.strings() //
+						  .withCharRange('a', 'z') //
+						  .ofMinLength(1) //
+						  .ofMaxLength(10) //
+						  .filter(string -> string.endsWith("h"));
+	}
+
+	@Provide
+	Arbitrary<String> second() {
+		return Arbitraries.strings()
+						  .withCharRange('0', '9')
+						  .ofMinLength(0)
+						  .ofMaxLength(10)
+						  .filter(string -> string.length() >= 1);
+	}
+
+	@Property(reporting = Reporting.GENERATED)
+	boolean shouldShrinkToAAH(@ForAll("aVariableString") String aString) {
+		return aString.length() > 4 || aString.length() < 3;
+	}
+
+	@Provide
+	Arbitrary<String> aVariableString() {
+		return Arbitraries.strings() //
+						  .withCharRange('a', 'z') //
+						  .ofMinLength(1) //
+						  .ofMaxLength(10) //
+						  .filter(string -> string.endsWith("h"));
+//						  .filter(string -> string.charAt(0) > 'a');
+	}
+
+}
