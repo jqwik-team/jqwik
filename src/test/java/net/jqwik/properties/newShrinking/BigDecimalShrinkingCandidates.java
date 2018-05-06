aa42e6dee0ffa613b940bc517acaf58271fca1c7
Johannes Link <business@johanneslink.net>
2018-05-06 22:24:17 +0200

    Migrated BigDecimalShrinkingCandidates to new shrinking

---
+++ src/test/java/net/jqwik/properties/newShrinking/BigDecimalShrinkingCandidates.java
@@ -0,0 +1,55 @@
+package net.jqwik.properties.newShrinking;
+
+import net.jqwik.properties.arbitraries.*;
+
+import java.math.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class BigDecimalShrinkingCandidates implements ShrinkingCandidates<BigDecimal> {
+	private final Range<BigDecimal> range;
+	private final BigIntegerShrinkingCandidates integralShrinkCandidates;
+
+	public BigDecimalShrinkingCandidates(Range<BigDecimal> range, BigDecimal target) {
+		this.range = range;
+		this.integralShrinkCandidates = new BigIntegerShrinkingCandidates(target.toBigInteger());
+	}
+
+	@Override
+	public Set<BigDecimal> candidatesFor(BigDecimal value) {
+		Set<BigDecimal> candidates = new HashSet<>();
+		if (hasDecimals(value))
+			candidates.addAll(shrinkDecimals(value));
+		candidates.addAll(shrinkIntegral(value));
+		return candidates;
+	}
+
+	private Set<BigDecimal> shrinkDecimals(BigDecimal value) {
+		Set<BigDecimal> shrunkDecimals = new HashSet<>();
+		range.ifIncluded(roundOneDigitDown(value), shrunkDecimals::add);
+		range.ifIncluded(roundOneDigitUp(value), shrunkDecimals::add);
+		return shrunkDecimals;
+	}
+
+	private BigDecimal roundOneDigitUp(BigDecimal value) {
+		return value.setScale(value.scale() -1, BigDecimal.ROUND_UP);
+	}
+
+	private BigDecimal roundOneDigitDown(BigDecimal value) {
+		return value.setScale(value.scale() -1, BigDecimal.ROUND_DOWN);
+	}
+
+	private Set<BigDecimal> shrinkIntegral(BigDecimal value) {
+		return integralShrinkCandidates.candidatesFor(value.toBigInteger()) //
+									   .stream() //
+									   .map(BigDecimal::new) //
+									   .collect(Collectors.toSet());
+	}
+
+	private boolean hasDecimals(BigDecimal value) {
+		if (value.scale() <= 0)
+			return false;
+		return value.remainder(BigDecimal.ONE).compareTo(BigDecimal.ZERO) != 0;
+	}
+
+}
---
+++ src/test/java/net/jqwik/properties/newShrinking/BigDecimalShrinkingCandidatesTests.java
@@ -0,0 +1,80 @@
+package net.jqwik.properties.newShrinking;
+
+import net.jqwik.api.*;
+import net.jqwik.properties.arbitraries.*;
+
+import java.math.*;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Group
+class BigDecimalShrinkingCandidatesTests {
+
+	@Example
+	void shrinkFrom0DoesNotShrink() {
+		ShrinkingCandidates<BigDecimal> shrinker = new BigDecimalShrinkingCandidates(Range.of(-10.0, 10.0).map(BigDecimal::new), BigDecimal.ZERO);
+		assertThat(shrinker.candidatesFor(BigDecimal.ZERO)).isEmpty();
+	}
+
+	@Example
+	void shrinkByRemovingDecimalsAndShrinkingIntegralPart() {
+		ShrinkingCandidates<BigDecimal> shrinker = new BigDecimalShrinkingCandidates(Range.of(-10.0, 10.0).map(BigDecimal::new), BigDecimal.ZERO);
+		Set<BigDecimal> candidates = shrinker.candidatesFor(new BigDecimal("2.15"));
+
+		assertThat(candidates).containsOnly( //
+			new BigDecimal("0"), //
+			new BigDecimal("1"), //
+			new BigDecimal("2.1"), //
+			new BigDecimal("2.2") //
+		);
+	}
+
+	@Example
+	void shrinkWillNotShrinkToDecimalsOutsideRange() {
+		ShrinkingCandidates<BigDecimal> shrinker = new BigDecimalShrinkingCandidates(Range.of(new BigDecimal("2.11"), new BigDecimal("10")), new BigDecimal("2.11"));
+		Set<BigDecimal> candidates = shrinker.candidatesFor(new BigDecimal("2.15"));
+
+		assertThat(candidates).containsOnly(new BigDecimal("2.2"));
+	}
+
+	@Example
+	void shrinkByRemovingLastDecimal() {
+		ShrinkingCandidates<BigDecimal> shrinker = new BigDecimalShrinkingCandidates(Range.of(-10.0, 10.0).map(BigDecimal::new), BigDecimal.ZERO);
+		Set<BigDecimal> candidates = shrinker.candidatesFor(new BigDecimal("2.1"));
+
+		assertThat(candidates).contains( //
+			new BigDecimal("2"), //
+			new BigDecimal("3") //
+		);
+	}
+
+	@Example
+	void shrinkNegativeByRemovingDecimalsAndShrinkingIntegral() {
+		ShrinkingCandidates<BigDecimal> shrinker = new BigDecimalShrinkingCandidates(Range.of(-10.0, 10.0).map(BigDecimal::new), BigDecimal.ZERO);
+		Set<BigDecimal> candidates = shrinker.candidatesFor(new BigDecimal("-3.99"));
+
+		assertThat(candidates).containsOnly( //
+			new BigDecimal("0"), //
+			new BigDecimal("-1"), //
+			new BigDecimal("-2"), //
+			new BigDecimal("-3.9"), //
+			new BigDecimal("-4.0") //
+		);
+	}
+
+	@Example
+	void withNoDecimalsShrinkLikeIntegrals() {
+		ShrinkingCandidates<BigDecimal> shrinker = new BigDecimalShrinkingCandidates(Range.of(-10.0, 10.0).map(BigDecimal::new), BigDecimal.ZERO);
+		Set<BigDecimal> candidates = shrinker.candidatesFor(new BigDecimal(5.0));
+
+		assertThat(candidates).containsOnly( //
+			new BigDecimal(0.0), //
+			new BigDecimal(1.0), //
+			new BigDecimal(2.0), //
+			new BigDecimal(3.0), //
+			new BigDecimal(4.0) //
+		);
+	}
+
+}
