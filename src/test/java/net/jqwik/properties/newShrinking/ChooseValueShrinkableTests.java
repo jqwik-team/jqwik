2487efd30475106ba7d7f5d487d2551d08f8c974
Johannes Link <business@johanneslink.net>
2018-05-10 15:03:03 +0200

    Done with new ChooseValueShrinkable

--- src/test/java/net/jqwik/properties/newShrinking/ChooseValueShrinkableTests.java
+++ src/test/java/net/jqwik/properties/newShrinking/ChooseValueShrinkableTests.java
@@ -22,56 +22,18 @@ class ChooseValueShrinkableTests {
 		assertThat(shrinkable.value()).isEqualTo(3);
 	}
 
-//	@Example
-//	void shrinking() {
-//		NShrinkable<Integer> integerShrinkable = new ShrinkableTypesForTest.OneStepShrinkable(3);
-//		NShrinkable<String> shrinkable = integerShrinkable.map(i -> String.valueOf(i) + String.valueOf(i));
-//
-//		ShrinkingSequence<String> sequence = shrinkable.shrink(ignore -> false);
-//
-//		assertThat(sequence.next(count, ignore -> {
-//		})).isTrue();
-//		assertThat(sequence.current().value()).isEqualTo("22");
-//		assertThat(sequence.next(count, ignore -> {
-//		})).isTrue();
-//		assertThat(sequence.current().value()).isEqualTo("11");
-//		assertThat(sequence.next(count, ignore -> {
-//		})).isTrue();
-//		assertThat(sequence.current().value()).isEqualTo("00");
-//		assertThat(sequence.next(count, ignore -> {
-//		})).isFalse();
-//		assertThat(sequence.current().value()).isEqualTo("00");
-//
-//		assertThat(counter.get()).isEqualTo(3);
-//	}
-//
-//
-//	@Example
-//	void reportFalsifier() {
-//
-//		Consumer<String> reporter = mock(Consumer.class);
-//
-//		NShrinkable<Integer> integerShrinkable = new ShrinkableTypesForTest.OneStepShrinkable(3);
-//		NShrinkable<String> shrinkable = integerShrinkable.map(i -> String.valueOf(i) + String.valueOf(i));
-//
-//		ShrinkingSequence<String> sequence = shrinkable.shrink(ignore -> false);
-//
-//		assertThat(sequence.next(count, reporter)).isTrue();
-//		assertThat(sequence.current().value()).isEqualTo("22");
-//		verify(reporter).accept("22");
-//
-//
-//		assertThat(sequence.next(count, reporter)).isTrue();
-//		assertThat(sequence.current().value()).isEqualTo("11");
-//		verify(reporter).accept("11");
-//
-//		assertThat(sequence.next(count, reporter)).isTrue();
-//		assertThat(sequence.current().value()).isEqualTo("00");
-//		verify(reporter).accept("00");
-//
-//		assertThat(sequence.next(count, reporter)).isFalse();
-//		verifyNoMoreInteractions(reporter);
-//
-//	}
+	@Example
+	void shrinking() {
+		NShrinkable<Integer> shrinkable = new ChooseValueShrinkable<>(4, Arrays.asList(1, 2, 3, 4, 5));
 
+		ShrinkingSequence<Integer> sequence = shrinkable.shrink(ignore -> false);
+
+		assertThat(sequence.next(count, reporter)).isTrue();
+		assertThat(sequence.current().value()).isEqualTo(1);
+		verify(reporter).accept(1);
+
+		assertThat(sequence.next(count, reporter)).isFalse();
+		assertThat(counter.get()).isEqualTo(1);
+		verifyNoMoreInteractions(reporter);
+	}
 }
