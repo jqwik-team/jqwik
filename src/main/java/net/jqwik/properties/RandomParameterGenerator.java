6bfccda6c6b6b01dc695e0770146cb57dcee40f5
Johannes Link <business@johanneslink.net>
2018-07-11 10:52:28 +0200

    Can handle type variable as parameter (but not in parameterized type)

--- src/main/java/net/jqwik/properties/RandomParameterGenerator.java
+++ src/main/java/net/jqwik/properties/RandomParameterGenerator.java
@@ -15,8 +15,18 @@ public class RandomParameterGenerator {
 		this.generators = new ArrayList<>(generators);
 	}
 
-	public Shrinkable next(Random random) {
+	public Shrinkable next(Random random, Map<TypeUsage, RandomGenerator> generatorsCache) {
+		RandomGenerator selectedGenerator = selectGenerator(random, generatorsCache);
+		return selectedGenerator.next(random);
+	}
+
+	private RandomGenerator selectGenerator(Random random, Map<TypeUsage, RandomGenerator> generatorsCache) {
+		if (generatorsCache.containsKey(typeUsage)) {
+			return generatorsCache.get(typeUsage);
+		}
 		int index = generators.size() == 1 ? 0 : random.nextInt(generators.size());
-		return generators.get(index).next(random);
+		RandomGenerator selectedGenerator = generators.get(index);
+		generatorsCache.put(typeUsage, selectedGenerator);
+		return selectedGenerator;
 	}
 }
