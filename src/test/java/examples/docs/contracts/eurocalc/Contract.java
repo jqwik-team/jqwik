3496068764a191e239d79dc4c58448d16eecd9de
Johannes Link <business@johanneslink.net>
2018-07-18 10:01:31 +0200

    Modified the contract example

---
+++ src/test/java/examples/docs/contracts/eurocalc/Contract.java
@@ -0,0 +1,18 @@
+package examples.docs.contracts.eurocalc;
+
+public @interface Contract {
+
+	Class value();
+
+	@interface Precondition {
+
+	}
+
+	@interface Postcondition {
+
+	}
+
+	@interface Invariant {
+
+	}
+}
--- src/test/java/examples/docs/contracts/eurocalc/ContractRegistry.java
+++
@@ -1,9 +0,0 @@
-package examples.docs.contracts.eurocalc;
-
-import net.jqwik.api.*;
-
-public class ContractRegistry {
-	public static <T1, T2> void addPrecondition(Class<?> target, String methodName, Combinators.F2<T2, T2, Void> precondition) {
-
-	}
-}
