51adbf823ac3cb660f8b3cd280c1ef3a151a420b
Johannes Link <business@johanneslink.net>
2018-07-19 15:59:25 +0200

    Some javadoc

--- src/main/java/net/jqwik/api/lifecycles/PropertyFinallyLifecycle.java
+++ src/main/java/net/jqwik/api/lifecycles/PropertyFinallyLifecycle.java
@@ -1,5 +1,8 @@
 package net.jqwik.api.lifecycles;
 
+/**
+ * Experimental feature. Not ready for public usage yet.
+ */
 public interface PropertyFinallyLifecycle {
 	void finallyAfterProperty(PropertyLifecycleContext propertyDescriptor) throws Throwable;
 }
--- src/main/java/net/jqwik/api/lifecycles/PropertyLifecycleContext.java
+++ src/main/java/net/jqwik/api/lifecycles/PropertyLifecycleContext.java
@@ -2,6 +2,9 @@ package net.jqwik.api.lifecycles;
 
 import java.lang.reflect.Method;
 
+/**
+ * Experimental feature. Not ready for public usage yet.
+ */
 public interface PropertyLifecycleContext {
 
 	Method targetMethod();
