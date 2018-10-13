924d7a6210490da75ad698ebce18dc41a2c9f0a1
Johannes Link <business@johanneslink.net>
2018-10-13 13:40:41 +0200

    Implemented exhaustive generation for Arbitraries.of()

--- src/test/java/examples/docs/ExhaustiveGenerationExamples.java
+++ src/test/java/examples/docs/ExhaustiveGenerationExamples.java
@@ -11,4 +11,14 @@ class ExhaustiveGenerationExamples {
 	@Report(Reporting.GENERATED)
 	void allNumbersFrom1to10(@ForAll @IntRange(min = 1, max = 10) int anInt) {
 	}
+
+	@Property(generation = EXHAUSTIVE)
+	@Report(Reporting.GENERATED)
+	void allPairsOfBooleanAndSamples(@ForAll boolean aBool, @ForAll("samples") String sample) {
+	}
+
+	@Provide
+	Arbitrary<String> samples() {
+		return Arbitraries.of("A", "B", "C");
+	}
 }
