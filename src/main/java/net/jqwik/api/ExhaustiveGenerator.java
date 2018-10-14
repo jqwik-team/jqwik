fdcdc386717e468a9593830adb9d9a7ca8be00f8
Johannes Link <business@johanneslink.net>
2018-10-14 13:09:32 +0200

    Changed supertype of ExhaustiveGenerator to Iterable

--- src/main/java/net/jqwik/api/ExhaustiveGenerator.java
+++ src/main/java/net/jqwik/api/ExhaustiveGenerator.java
@@ -1,13 +1,6 @@
 package net.jqwik.api;
 
-import java.util.*;
-import java.util.function.*;
-import java.util.stream.*;
-
-import net.jqwik.properties.arbitraries.*;
-import net.jqwik.properties.shrinking.*;
-
-public interface ExhaustiveGenerator<T> extends Iterator<T> {
+public interface ExhaustiveGenerator<T> extends Iterable<T> {
 
 	/**
 	 * @return the maximum number of values that will be generated
