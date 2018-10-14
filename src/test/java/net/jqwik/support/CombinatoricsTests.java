929c92a5d219c659702d26e4dad0fb59c31951e7
Johannes Link <business@johanneslink.net>
2018-10-14 20:09:13 +0200

    Combinatorics.combine works for two iterables

--- src/test/java/net/jqwik/support/CombinatoricsTests.java
+++ src/test/java/net/jqwik/support/CombinatoricsTests.java
@@ -1,6 +1,7 @@
 package net.jqwik.support;
 
 import java.util.*;
+import java.util.stream.*;
 
 import org.assertj.core.api.*;
 
@@ -20,6 +21,21 @@ class CombinatoricsTests {
 
 		Assertions.assertThat(iterator).containsExactly(
 			asList(1), asList(2), asList(3)
+		);
+	}
+
+	@Example
+	void combineTwoIterables() {
+		List<Iterable> iterables = asList(
+			asList(1, 2, 3),
+			asList('a', 'b')
+		);
+		Iterator<List> iterator = Combinatorics.combine(iterables);
+
+		Assertions.assertThat(iterator).containsExactly(
+			asList(1, 'a'), asList(1, 'b'),
+			asList(2, 'a'), asList(2, 'b'),
+			asList(3, 'a'), asList(3, 'b')
 		);
 	}
 }
