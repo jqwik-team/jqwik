30414db6827e6a1d69520d72e6feaf7034133e5d
Johannes Link <business@johanneslink.net>
2018-10-14 20:17:56 +0200

    Added failing test for Combinatorics.combine

--- src/test/java/net/jqwik/support/CombinatoricsTests.java
+++ src/test/java/net/jqwik/support/CombinatoricsTests.java
@@ -8,6 +8,7 @@ import org.assertj.core.api.*;
 import net.jqwik.api.*;
 
 import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
 class CombinatoricsTests {
@@ -19,7 +20,7 @@ class CombinatoricsTests {
 		);
 		Iterator<List> iterator = Combinatorics.combine(iterables);
 
-		Assertions.assertThat(iterator).containsExactly(
+		assertThat(iterator).containsExactly(
 			asList(1), asList(2), asList(3)
 		);
 	}
@@ -32,10 +33,47 @@ class CombinatoricsTests {
 		);
 		Iterator<List> iterator = Combinatorics.combine(iterables);
 
-		Assertions.assertThat(iterator).containsExactly(
+		assertThat(iterator).containsExactly(
 			asList(1, 'a'), asList(1, 'b'),
 			asList(2, 'a'), asList(2, 'b'),
 			asList(3, 'a'), asList(3, 'b')
 		);
+	}
+
+	@Example
+	void twoIterablesFirstIsSingleton() {
+		List<Iterable> iterables = asList(
+			asList(1),
+			asList('a', 'b')
+		);
+		Iterator<List> iterator = Combinatorics.combine(iterables);
+
+		assertThat(iterator).containsExactly(
+			asList(1, 'a'), asList(1, 'b')
+		);
+	}
+
+	@Example
+	void twoIterablesSecondIsSingleton() {
+		List<Iterable> iterables = asList(
+			asList(1, 2, 3),
+			asList('a')
+		);
+		Iterator<List> iterator = Combinatorics.combine(iterables);
+
+		assertThat(iterator).containsExactly(
+			asList(1, 'a'), asList(2, 'a'), asList(3, 'a')
+		);
+	}
+
+	@Example
+	void twoIterablesOneIsEmpty() {
+		List<Iterable> iterables = asList(
+			asList(1, 2, 3),
+			Collections.emptyList()
+		);
+		Iterator<List> iterator = Combinatorics.combine(iterables);
+		assertThat(iterator.hasNext()).isFalse();
+		assertThat(iterator).isEmpty();
 	}
 }
