8be02ef31943175c7ffa11f5590a23c1ac293a22
Johannes Link <business@johanneslink.net>
2018-10-17 17:16:44 +0200

    Added Combinatorics.setCombinations

--- src/main/java/net/jqwik/support/Combinatorics.java
+++ src/main/java/net/jqwik/support/Combinatorics.java
@@ -3,6 +3,8 @@ package net.jqwik.support;
 import java.util.*;
 import java.util.concurrent.atomic.*;
 
+import static java.util.Arrays.asList;
+
 public class Combinatorics {
 
 	@SuppressWarnings("unchecked")
@@ -14,7 +16,11 @@ public class Combinatorics {
 	}
 
 	private static <T> Iterator<List<T>> emptyListSingleton() {
-		return Arrays.asList((List<T>) new ArrayList()).iterator();
+		return asList((List<T>) new ArrayList()).iterator();
+	}
+
+	private static <T> Iterator<Set<T>> emptySetSingleton() {
+		return asList((Set<T>) new HashSet<>()).iterator();
 	}
 
 	public static <T> Iterator<List<T>> listCombinations(Iterable<T> elementIterable, int minSize, int maxSize) {
@@ -25,13 +31,37 @@ public class Combinatorics {
 		return concat(iterators);
 	}
 
-	private static <T> Iterator<List<T>> concat(List<Iterator<List<T>>> iterators) {
-		return new Iterator<List<T>>() {
+	private static <T> Iterator<List<T>> listIterator(Iterable<T> elementIterable, int listSize) {
+		List<Iterable<T>> listOfIterables = new ArrayList<>();
+		for (int i = 0; i < listSize; i++) {
+			listOfIterables.add(elementIterable);
+		}
+		return combine(listOfIterables);
+	}
+
+	public static <T> Iterator<Set<T>> setCombinations(Iterable<T> elementIterable, int minSize, int maxSize) {
+		List<Iterator<Set<T>>> iterators = new ArrayList<>();
+		for(int setSize = minSize; setSize <= maxSize; setSize++) {
+			Iterator<Set<T>> setIterator = setIterator(elementIterable, setSize);
+			iterators.add(setIterator);
+		}
+		return concat(iterators);
+	}
+
+	private static <T> Iterator<Set<T>> setIterator(Iterable<T> elementIterable, int setSize) {
+		if (setSize == 0) {
+			return emptySetSingleton();
+		}
+		return new SetIterator<>(elementIterable, setSize);
+	}
+
+	private static <T> Iterator<T> concat(List<Iterator<T>> iterators) {
+		return new Iterator<T>() {
 
 			AtomicInteger position = new AtomicInteger(0);
-			Iterator<List<T>> next = findNext();
+			Iterator<T> next = findNext();
 
-			private Iterator<List<T>> findNext() {
+			private Iterator<T> findNext() {
 				while (!iterators.get(position.get()).hasNext()) {
 					if (position.get() >= iterators.size() -1)
 						return null;
@@ -46,23 +76,15 @@ public class Combinatorics {
 			}
 
 			@Override
-			public List<T> next() {
+			public T next() {
 				if (!hasNext()) {
 					throw new NoSuchElementException();
 				}
-				List<T> current = next.next();
+				T current = next.next();
 				next = findNext();
 				return current;
 			}
 		};
-	}
-
-	private static <T> Iterator<List<T>> listIterator(Iterable<T> elementIterable, int listSize) {
-		List<Iterable<T>> listOfIterables = new ArrayList<>();
-		for (int i = 0; i < listSize; i++) {
-			listOfIterables.add(elementIterable);
-		}
-		return combine(listOfIterables);
 	}
 
 	private static class CombinedIterator<T> implements Iterator<List<T>> {
@@ -112,6 +134,51 @@ public class Combinatorics {
 			List rest = new ArrayList(tail);
 			rest.add(0, head);
 			return rest;
+		}
+	}
+
+	private static class SetIterator<T> implements Iterator<Set<T>> {
+
+		private final Iterator<List<T>> combinedListIterator;
+		private final Set<Set<T>> generatedSets = new HashSet<>();
+		private final int setSize;
+		private Set<T> next;
+
+		private SetIterator(Iterable<T> elementIterable, int setSize) {
+			this.setSize = setSize;
+			List<Iterable<T>> iterables = new ArrayList<>();
+			for (int i = 0; i < setSize; i++) {
+				iterables.add(elementIterable);
+			}
+			combinedListIterator = new CombinedIterator<>(iterables);
+			next = findNext();
+		}
+
+		private Set<T> findNext() {
+			while (combinedListIterator.hasNext()) {
+				HashSet<T> candidate = new HashSet<>(combinedListIterator.next());
+				if (candidate.size() != setSize || generatedSets.contains(candidate)) {
+					continue;
+				}
+				generatedSets.add(candidate);
+				return candidate;
+			}
+			return null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return next != null;
+		}
+
+		@Override
+		public Set<T> next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			Set<T> current = next;
+			next = findNext();
+			return current;
 		}
 	}
 }
--- src/test/java/net/jqwik/support/CombinatoricsTests.java
+++ src/test/java/net/jqwik/support/CombinatoricsTests.java
@@ -9,6 +9,7 @@ import net.jqwik.api.constraints.*;
 import static java.util.Arrays.*;
 import static org.assertj.core.api.Assertions.*;
 
+@SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
 @Label("Combinatorics")
 class CombinatoricsTests {
 
@@ -91,6 +92,84 @@ class CombinatoricsTests {
 	}
 
 	@Group
+	@Label("setCombinations")
+	class CombineSet {
+
+		@Example
+		void emptySetOnly() {
+			Iterable<Integer> elementIterable = asList(1, 2, 3);
+			Iterator<Set<Integer>> iterator = Combinatorics.setCombinations(elementIterable, 0, 0);
+
+			assertThat(iterator).containsExactly(
+				asSet()
+			);
+		}
+
+		private Set<Integer> asSet(Integer... integers) {
+			return new HashSet<>(asList(integers));
+		}
+
+		@Example
+		void setOfSize1() {
+			Iterable<Integer> elementIterable = asList(1, 2, 3);
+			Iterator<Set<Integer>> iterator = Combinatorics.setCombinations(elementIterable, 1, 1);
+
+			assertThat(iterator).containsExactly(
+				asSet(1),
+				asSet(2),
+				asSet(3)
+			);
+		}
+
+		@Example
+		void setOfSize3() {
+			Iterable<Integer> elementIterable = asList(1, 2, 3, 4, 1, 1);
+			Iterator<Set<Integer>> iterator = Combinatorics.setCombinations(elementIterable, 3, 3);
+
+			assertThat(iterator).containsExactly(
+				asSet(1, 2, 3),
+				asSet(1, 2, 4),
+				asSet(1, 3, 4),
+				asSet(2, 3, 4)
+			);
+		}
+
+		@Example
+		void elementsCannotFillSet() {
+			Iterable<Integer> elementIterable = asList(1, 2);
+			Iterator<Set<Integer>> iterator = Combinatorics.setCombinations(elementIterable, 3, 3);
+
+			assertThat(iterator).isEmpty();
+		}
+
+		@Example
+		void setOfSizeUpTo1() {
+			Iterable<Integer> elementIterable = asList(1, 2, 3);
+			Iterator<Set<Integer>> iterator = Combinatorics.setCombinations(elementIterable, 0, 1);
+
+			assertThat(iterator).containsExactly(
+				asSet(),
+				asSet(1),
+				asSet(2),
+				asSet(3)
+			);
+		}
+
+		@Example
+		void setOfSize2to3() {
+			Iterable<Integer> elementIterable = asList(1, 2, 3);
+			Iterator<Set<Integer>> iterator = Combinatorics.setCombinations(elementIterable, 2, 3);
+
+			assertThat(iterator).containsExactly(
+				asSet(1, 2),
+				asSet(1, 3),
+				asSet(2, 3),
+				asSet(1, 2, 3)
+			);
+		}
+	}
+
+	@Group
 	@Label("combine")
 	class Combine {
 		@Example
@@ -98,49 +177,49 @@ class CombinatoricsTests {
 			List<Iterable<Integer>> iterables = asList();
 			Iterator<List<Integer>> iterator = Combinatorics.combine(iterables);
 
-		assertThat(iterator).containsExactly(
-			asList()
-		);
-	}
+			assertThat(iterator).containsExactly(
+				asList()
+			);
+		}
 
-	@Example
-	void combineSingleIterable() {
-		List<Iterable<Integer>> iterables = asList(
-			asList(1, 2, 3)
-		);
-		Iterator<List<Integer>> iterator = Combinatorics.combine(iterables);
+		@Example
+		void combineSingleIterable() {
+			List<Iterable<Integer>> iterables = asList(
+				asList(1, 2, 3)
+			);
+			Iterator<List<Integer>> iterator = Combinatorics.combine(iterables);
 
-		assertThat(iterator).containsExactly(
-			asList(1), asList(2), asList(3)
-		);
-	}
+			assertThat(iterator).containsExactly(
+				asList(1), asList(2), asList(3)
+			);
+		}
 
-	@Example
-	void failsWithNoSuchElementException() {
-		List<Iterable<Integer>> iterables = asList(
-			asList(1)
-		);
-		Iterator<List<Integer>> iterator = Combinatorics.combine(iterables);
-		iterator.next();
+		@Example
+		void failsWithNoSuchElementException() {
+			List<Iterable<Integer>> iterables = asList(
+				asList(1)
+			);
+			Iterator<List<Integer>> iterator = Combinatorics.combine(iterables);
+			iterator.next();
 
-		assertThat(iterator.hasNext()).isFalse();
-		assertThatThrownBy(() -> iterator.next()).isInstanceOf(NoSuchElementException.class);
-	}
+			assertThat(iterator.hasNext()).isFalse();
+			assertThatThrownBy(() -> iterator.next()).isInstanceOf(NoSuchElementException.class);
+		}
 
-	@Example
-	void combineTwoIterables() {
-		List<Iterable<Object>> iterables = asList(
-			asList(1, 2, 3),
-			asList('a', 'b')
-		);
-		Iterator<List<Object>> iterator = Combinatorics.combine(iterables);
+		@Example
+		void combineTwoIterables() {
+			List<Iterable<Object>> iterables = asList(
+				asList(1, 2, 3),
+				asList('a', 'b')
+			);
+			Iterator<List<Object>> iterator = Combinatorics.combine(iterables);
 
-		assertThat(iterator).containsExactly(
-			asList(1, 'a'), asList(1, 'b'),
-			asList(2, 'a'), asList(2, 'b'),
-			asList(3, 'a'), asList(3, 'b')
-		);
-	}
+			assertThat(iterator).containsExactly(
+				asList(1, 'a'), asList(1, 'b'),
+				asList(2, 'a'), asList(2, 'b'),
+				asList(3, 'a'), asList(3, 'b')
+			);
+		}
 
 		@Example
 		void twoIterablesFirstIsSingleton() {
@@ -201,9 +280,9 @@ class CombinatoricsTests {
 					.reduce(((left, right) -> left * right)).orElse(1);
 
 			List<Iterable<Integer>> iterables = listOfLists
-										   .stream()
-										   .map(aList -> (Iterable<Integer>) aList)
-										   .collect(Collectors.toList());
+				.stream()
+				.map(aList -> (Iterable<Integer>) aList)
+				.collect(Collectors.toList());
 
 			Iterator<List<Integer>> iterator = Combinatorics.combine(iterables);
 			assertThat(iterator).hasSize(product);
@@ -212,9 +291,9 @@ class CombinatoricsTests {
 		@Provide
 		Arbitrary<List<List<Integer>>> iterables() {
 			return Arbitraries
-					   .integers().between(-1000, 1000).unique()
-					   .list().ofMaxSize(15)
-					   .list();
+				.integers().between(-1000, 1000).unique()
+				.list().ofMaxSize(15)
+				.list();
 		}
 	}
 }
