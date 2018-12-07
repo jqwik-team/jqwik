bb08df6f83c2222a9091c1f36b150859177596a1
Johannes Link <business@johanneslink.net>
2018-12-07 10:52:13 +0100

    Introduced ShrinkableFactoryFacade

--- api/src/main/java/net/jqwik/api/Shrinkable.java
+++ api/src/main/java/net/jqwik/api/Shrinkable.java
@@ -1,13 +1,29 @@
 package net.jqwik.api;
 
-import net.jqwik.engine.properties.shrinking.*;
-
 import java.util.function.*;
 
 public interface Shrinkable<T> extends Comparable<Shrinkable<T>> {
 
+	abstract class ShrinkableFactoryFacade {
+		private static final String SHRINKABLE_FACTORY_FACADE_IMPL = "net.jqwik.engine.facades.ShrinkableFactoryFacadeImpl";
+		private static ShrinkableFactoryFacade implementation;
+
+		static  {
+			try {
+				implementation = (ShrinkableFactoryFacade) Class.forName(SHRINKABLE_FACTORY_FACADE_IMPL).newInstance();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public abstract <T> Shrinkable<T> unshrinkable(T value);
+		public abstract <T, U> Shrinkable<U> map(Shrinkable<T> self, Function<T, U> mapper);
+		public abstract <T> Shrinkable<T> filter(Shrinkable<T> self, Predicate<T> filter);
+		public abstract <T, U> Shrinkable<U> flatMap(Shrinkable<T> self, Function<T, Arbitrary<U>> flatMapper, int tries, long randomSeed);
+	}
+
 	static <T> Shrinkable<T> unshrinkable(T value) {
-		return new Unshrinkable<>(value);
+		return ShrinkableFactoryFacade.implementation.unshrinkable(value);
 	}
 
 	T value();
@@ -17,11 +33,11 @@ public interface Shrinkable<T> extends Comparable<Shrinkable<T>> {
 	ShrinkingDistance distance();
 
 	default <U> Shrinkable<U> map(Function<T, U> mapper) {
-		return new MappedShrinkable<>(this, mapper);
+		return ShrinkableFactoryFacade.implementation.map(this, mapper);
 	}
 
 	default Shrinkable<T> filter(Predicate<T> filter) {
-		return new FilteredShrinkable<>(this, filter);
+		return ShrinkableFactoryFacade.implementation.filter(this, filter);
 	}
 
 	@Override
@@ -34,6 +50,6 @@ public interface Shrinkable<T> extends Comparable<Shrinkable<T>> {
 	}
 
 	default <U> Shrinkable<U> flatMap(Function<T, Arbitrary<U>> flatMapper, int tries, long randomSeed) {
-		return new FlatMappedShrinkable<>(this, flatMapper, tries, randomSeed);
+		return ShrinkableFactoryFacade.implementation.flatMap(this, flatMapper, tries, randomSeed);
 	}
 }
