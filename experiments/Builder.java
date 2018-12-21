928a568e73220cc81626db1edaca99188cd42b00
Johannes Link <business@johanneslink.net>
2018-12-21 15:25:38 +0100

    Added builder idea to experiments

---
+++ experiments/Builder.java
@@ -0,0 +1,36 @@
+package net.jqwik.api;
+
+import java.util.*;
+import java.util.function.*;
+
+public class Builder<T> {
+
+	private final List<Function<T, T>> transformers = new ArrayList<>();
+
+	public static <T> Builder<T> from(Arbitrary<T> arbitrary) {
+		return new Builder<T>(arbitrary);
+	}
+
+	private final Arbitrary<T> arbitrary;
+
+	public Builder(Arbitrary<T> arbitrary) {this.arbitrary = arbitrary;}
+
+	public T build(Random random) {
+		RandomGenerator<T> generator = arbitrary.generator(1);
+		T value = generator.next(random).value();
+		return transform(value, transformers);
+	}
+
+	private T transform(T value, List<Function<T, T>> transformers) {
+		if (transformers.isEmpty()) {
+			return value;
+		}
+		T transformedValue = transformers.remove(0).apply(value);
+		return transform(transformedValue, transformers);
+	}
+
+	public Builder<T> with(Function<T, T> transformer) {
+		transformers.add(transformer);
+		return this;
+	}
+}
