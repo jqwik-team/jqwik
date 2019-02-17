8a9705fefa46dacd568049686334989b5d1cc31f
Johannes Link <business@johanneslink.net>
2019-02-17 10:52:20 +0100

    Made old architecture test work again

--- TODO.md
+++ TODO.md
@@ -4,5 +4,3 @@
   
 - TupleArbitraryProvider
 
-- Repair ArchitectureTests
-
--- engine/src/test/java/net/jqwik/ArchitectureTests.java
+++ engine/src/test/java/net/jqwik/ArchitectureTests.java
@@ -1,21 +1,35 @@
 package net.jqwik;
 
+import com.tngtech.archunit.core.importer.ImportOption.*;
 import com.tngtech.archunit.junit.*;
 import com.tngtech.archunit.lang.*;
 import com.tngtech.archunit.library.dependencies.*;
 
-@AnalyzeClasses(packages = "net.jqwik")
+@AnalyzeClasses(packages = "net.jqwik", importOptions = {DontIncludeTests.class, DontIncludeJars.class})
 public class ArchitectureTests {
 
 	//@ArchTest
 	//TODO: Does not work yet
 	public static final ArchRule noCyclicDependenciesInEnginePackages = SlicesRuleDefinition
 																			.slices()
+																			.matching("net.jqwik.engine.(**)")
+																			.should().notDependOnEachOther();
+
+	//@ArchTest
+	//TODO: Does not work yet
+	public static final ArchRule noCyclicDependenciesInEngineClasses = SlicesRuleDefinition
+																			.slices()
 																			.matching("net.jqwik.engine.(**)..")
 																			.should().beFreeOfCycles();
+
 	//@ArchTest
-	//TODO: Currently fails because of tests in net.jqwik.api
+	//TODO: Does not work yet
 	public static final ArchRule noCyclicDependenciesInApiPackages = SlicesRuleDefinition
+																		 .slices()
+																		 .matching("net.jqwik.api.(**)")
+																		 .should().notDependOnEachOther();
+	@ArchTest
+	public static final ArchRule noCyclicDependenciesInApiClasses = SlicesRuleDefinition
 																		 .slices()
 																		 .matching("net.jqwik.api.(**)..")
 																		 .should().beFreeOfCycles();
