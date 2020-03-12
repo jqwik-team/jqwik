0518314a46af790b5e1937bf5d1a5a9c114a430f
Johannes Link <business@johanneslink.net>
2020-03-12 15:11:38 +0100

    Added failing test for parameter resolution in beforeTry methods

---
+++ engine/src/test/java/net/jqwik/engine/properties/ResolvingParametersEverywhereTests.java
@@ -0,0 +1,41 @@
+package net.jqwik.engine.properties;
+
+import java.util.*;
+
+import org.assertj.core.api.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.lifecycle.*;
+
+@AddLifecycleHook(ResolveIntsTo42.class)
+class ResolvingParametersEverywhereTests {
+
+	@BeforeTry
+	void beforeProperty(int shouldBe42) {
+		Assertions.assertThat(shouldBe42).isEqualTo(42);
+	}
+
+	@Property(tries = 5)
+	void inProperty(int shouldBe42) {
+		Assertions.assertThat(shouldBe42).isEqualTo(42);
+	}
+}
+
+class ResolveIntsTo42 implements ResolveParameterHook {
+
+	@Override
+	public Optional<ParameterSupplier> resolve(ParameterResolutionContext parameterContext) {
+		if (parameterContext.typeUsage().isOfType(int.class)) {
+			return Optional.of(lifecycleContext -> {
+				return 42;
+			});
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public PropagationMode propagateTo() {
+		return PropagationMode.ALL_DESCENDANTS;
+	}
+}
+
