a16a13ee65a6c39adca8b648cde7d808d4530b4f
Johannes Link <business@johanneslink.net>
2020-04-16 09:37:14 +0200

    Implemented edgeCases() for SetArbitrary

--- engine/src/main/java/net/jqwik/engine/properties/arbitraries/ListArbitrary.java
+++ engine/src/main/java/net/jqwik/engine/properties/arbitraries/ListArbitrary.java
@@ -29,22 +29,7 @@ public class ListArbitrary<T> extends DefaultCollectionArbitrary<T, List<T>> {
 
 	@Override
 	public EdgeCases<List<T>> edgeCases() {
-		EdgeCases<List<T>> emptyEdgeCases = (minSize == 0) ? emptyListEdgeCase() : EdgeCases.none();
-		EdgeCases<List<T>> edgeCasesWithElements = (minSize <= 1) ? singleElementEdgeCases() : EdgeCases.none();
-		return EdgeCases.concat(emptyEdgeCases, edgeCasesWithElements);
+		return edgeCases(ShrinkableList::new);
 	}
 
-	private EdgeCases<List<T>> singleElementEdgeCases() {
-		return elementArbitrary.edgeCases()
-						.mapShrinkable((Shrinkable<T> shrinkableT) -> {
-							List<Shrinkable<T>> elements = Collections.singletonList(shrinkableT);
-							return new ShrinkableList<>(elements, minSize);
-						});
-	}
-
-	private EdgeCases<List<T>> emptyListEdgeCase() {
-		return EdgeCases.fromSupplier(
-			() -> new ShrinkableList<T>(Collections.emptyList(), minSize)
-		);
-	}
 }
