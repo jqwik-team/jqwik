4ed66ae270dde79329d412911a9bc89dce67b10e
Johannes Link <business@johanneslink.net>
2020-05-27 10:04:49 +0200

    Update javadoc for ResolveParameterHook

--- api/src/main/java/net/jqwik/api/lifecycle/ResolveParameterHook.java
+++ api/src/main/java/net/jqwik/api/lifecycle/ResolveParameterHook.java
@@ -7,12 +7,17 @@ import org.apiguardian.api.*;
 import static org.apiguardian.api.API.Status.*;
 
 /**
- * Experimental feature. Not ready for public usage yet.
+ * This hook type allows to provide parameters for property methods
+ * but also to annotated lifecycle methods.
  */
 @API(status = EXPERIMENTAL, since = "1.2.4")
 @FunctionalInterface
 public interface ResolveParameterHook extends LifecycleHook {
 
+	/**
+	 * A functional interface specialized on providing parameters to property methods
+	 * and to annotated lifecycle methods.
+	 */
 	@API(status = EXPERIMENTAL, since = "1.2.5")
 	@FunctionalInterface
 	interface ParameterSupplier {
@@ -20,7 +25,7 @@ public interface ResolveParameterHook extends LifecycleHook {
 		 * Supply the requested parameter. For the <em>same</em> {@code optionalTry} the <em>same</em>
 		 * object must be returned if this object has state that could change its behaviour.
 		 *
-		 * @param optionalTry Contains a {@linkplain TryLifecycleContext} instance if used to supply a property's parameter.
+		 * @param optionalTry Contains a {@linkplain TryLifecycleContext} instance if used to supply a property method's parameter.
 		 *                    Otherwise it's empty.
 		 * @return the freshly generated object or one stored for the same context
 		 */
@@ -31,15 +36,17 @@ public interface ResolveParameterHook extends LifecycleHook {
 	 * This method will be called only once per property, whereas the returned supplier's get method
 	 * is usually invoked for each try - and potentially more often during shrinking or when resolving
 	 * parameters in before/after methods.
+	 * The returned supplier should always return an equivalent object,
+	 * i.e. an object that behaves the same when used in the same way.
 	 *
 	 * @param parameterContext Contains information of parameter to resolve
-	 * @param lifecycleContext Is never of type {@linkplain TryLifecycleContext} at resolution time
-	 * @return a supplier that should always return an equivalent object,
-	 * i.e. an object that behaves the same when used in the same way.
+	 * @param lifecycleContext Can be of type {@linkplain ContainerLifecycleContext} or {@linkplain PropertyLifecycleContext}
+	 * @return supplier instance wrapped in {@linkplain Optional} or {@linkplain Optional#empty()} if it cannot be resolved
 	 */
 	@API(status = EXPERIMENTAL, since = "1.2.5")
 	Optional<ParameterSupplier> resolve(ParameterResolutionContext parameterContext, LifecycleContext lifecycleContext);
 
+	@API(status = INTERNAL)
 	ResolveParameterHook DO_NOT_RESOLVE = ((parameterContext, lifecycleContext) -> Optional.empty());
 
 }
