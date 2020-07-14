da5eb9cca93dec9150af358c9e904a887b5c487b
Johannes Link <business@johanneslink.net>
2020-07-14 16:28:03 +0200

    Refactoring ChooseValueShrinkableTests

--- engine/src/test/java/net/jqwik/engine/properties/shrinking/ChooseValueShrinkableTests.java
+++ engine/src/test/java/net/jqwik/engine/properties/shrinking/ChooseValueShrinkableTests.java
@@ -1,7 +1,6 @@
 package net.jqwik.engine.properties.shrinking;
 
 import java.util.*;
-import java.util.concurrent.atomic.*;
 import java.util.function.*;
 
 import net.jqwik.api.*;
@@ -10,13 +9,12 @@ import net.jqwik.engine.properties.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import static net.jqwik.api.ShrinkingTestHelper.*;
+
 class ChooseValueShrinkableTests {
 
-	private AtomicInteger counter = new AtomicInteger(0);
-	private Runnable count = counter::incrementAndGet;
 	@SuppressWarnings("unchecked")
 	private Consumer<Integer> valueReporter = mock(Consumer.class);
-	private Consumer<FalsificationResult<Integer>> reporter = result -> valueReporter.accept(result.value());
 
 	@Example
 	void creation() {
@@ -28,15 +26,9 @@ class ChooseValueShrinkableTests {
 	@Example
 	void shrinking() {
 		Shrinkable<Integer> shrinkable = new ChooseValueShrinkable<>(4, Arrays.asList(1, 2, 3, 4, 5));
-
-		ShrinkingSequence<Integer> sequence = shrinkable.shrink((TestingFalsifier<Integer>) ignore -> false);
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo(1);
+		Integer shrunkValue = shrinkToEnd(shrinkable, (TestingFalsifier<Integer>) ignore -> false, valueReporter, null);
+		assertThat(shrunkValue).isEqualTo(1);
 		verify(valueReporter).accept(1);
-
-		assertThat(sequence.next(count, reporter)).isFalse();
-		assertThat(counter.get()).isEqualTo(1);
 		verifyNoMoreInteractions(valueReporter);
 	}
 }
