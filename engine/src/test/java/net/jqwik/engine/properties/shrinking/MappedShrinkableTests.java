fb95a746430102b5e966bc4b1c25f48825246567
Johannes Link <business@johanneslink.net>
2020-07-14 19:01:55 +0200

    Refactoring MappedShrinkableTests

--- engine/src/test/java/net/jqwik/engine/properties/shrinking/MappedShrinkableTests.java
+++ engine/src/test/java/net/jqwik/engine/properties/shrinking/MappedShrinkableTests.java
@@ -1,24 +1,21 @@
 package net.jqwik.engine.properties.shrinking;
 
-import java.util.concurrent.atomic.*;
 import java.util.function.*;
 
 import net.jqwik.api.*;
-import net.jqwik.engine.properties.*;
 import net.jqwik.engine.properties.shrinking.ShrinkableTypesForTest.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import static net.jqwik.api.ShrinkingTestHelper.*;
+
 @Group
 @Label("MappedShrinkable")
 class MappedShrinkableTests {
 
-	private AtomicInteger counter = new AtomicInteger(0);
-	private Runnable count = counter::incrementAndGet;
 	@SuppressWarnings("unchecked")
 	private Consumer<String> valueReporter = mock(Consumer.class);
-	private Consumer<FalsificationResult<String>> reporter = result -> valueReporter.accept(result.value());
 
 	@Example
 	void creation() {
@@ -33,20 +30,9 @@ class MappedShrinkableTests {
 		Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(3);
 		Shrinkable<String> shrinkable = integerShrinkable.map(i -> String.valueOf(i) + i);
 
-		ShrinkingSequence<String> sequence = shrinkable.shrink((TestingFalsifier<String>) ignore -> false);
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo("22");
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo("11");
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo("00");
-		assertThat(sequence.next(count, reporter)).isFalse();
-		assertThat(sequence.current().value()).isEqualTo("00");
-
-		assertThat(counter.get()).isEqualTo(3);
+		String shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), null);
+		assertThat(shrunkValue).isEqualTo("00");
 	}
-
 
 	@Example
 	void reportFalsifier() {
@@ -54,24 +40,13 @@ class MappedShrinkableTests {
 		Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(3);
 		Shrinkable<String> shrinkable = integerShrinkable.map(i -> String.valueOf(i) + i);
 
-		ShrinkingSequence<String> sequence = shrinkable.shrink((TestingFalsifier<String>) ignore -> false);
+		String shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), valueReporter, null);
+		assertThat(shrunkValue).isEqualTo("00");
 
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo("22");
 		verify(valueReporter).accept("22");
-
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo("11");
 		verify(valueReporter).accept("11");
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo("00");
 		verify(valueReporter).accept("00");
-
-		assertThat(sequence.next(count, reporter)).isFalse();
 		verifyNoMoreInteractions(valueReporter);
-
 	}
 
 }
