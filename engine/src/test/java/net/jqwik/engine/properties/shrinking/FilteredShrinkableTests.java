60a7bd9e3e6f7fdf1f4bee64aa67dfe240bcbbcd
Johannes Link <business@johanneslink.net>
2020-07-14 18:59:22 +0200

    Refactoring FilteredShrinkableTests

--- engine/src/test/java/net/jqwik/engine/properties/shrinking/FilteredShrinkableTests.java
+++ engine/src/test/java/net/jqwik/engine/properties/shrinking/FilteredShrinkableTests.java
@@ -1,25 +1,21 @@
 package net.jqwik.engine.properties.shrinking;
 
-import java.util.concurrent.atomic.*;
 import java.util.function.*;
 
 import net.jqwik.api.*;
-import net.jqwik.engine.properties.*;
 import net.jqwik.engine.properties.shrinking.ShrinkableTypesForTest.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import static net.jqwik.api.ShrinkingTestHelper.*;
+
 @Group
 @Label("FilteredShrinkable")
 class FilteredShrinkableTests {
 
-	private AtomicInteger counter = new AtomicInteger(0);
-	private Runnable count = counter::incrementAndGet;
-
 	@SuppressWarnings("unchecked")
 	private Consumer<Integer> valueReporter = mock(Consumer.class);
-	private Consumer<FalsificationResult<Integer>> reporter = result -> valueReporter.accept(result.value());
 
 	@Example
 	void creation() {
@@ -34,38 +30,20 @@ class FilteredShrinkableTests {
 		Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(3);
 		Shrinkable<Integer> shrinkable = integerShrinkable.filter(i -> i % 2 == 1);
 
-		ShrinkingSequence<Integer> sequence = shrinkable.shrink((TestingFalsifier<Integer>) ignore -> false);
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo(3);
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo(1);
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo(1);
-		assertThat(sequence.next(count, reporter)).isFalse();
-
-		assertThat(counter.get()).isEqualTo(3);
+		Integer shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), null);
+		assertThat(shrunkValue).isEqualTo(1);
 	}
-
 
 	@Example
 	void reportFalsifier() {
 		Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(3);
 		Shrinkable<Integer> shrinkable = integerShrinkable.filter(i -> i % 2 == 1);
 
-		ShrinkingSequence<Integer> sequence = shrinkable.shrink((TestingFalsifier<Integer>) ignore -> false);
+		Integer shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), valueReporter, null);
+		assertThat(shrunkValue).isEqualTo(1);
 
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo(3);
 		verify(valueReporter, never()).accept(3);
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).isEqualTo(1);
 		verify(valueReporter).accept(1);
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.next(count, reporter)).isFalse();
-
 		verifyNoMoreInteractions(valueReporter);
 	}
 
