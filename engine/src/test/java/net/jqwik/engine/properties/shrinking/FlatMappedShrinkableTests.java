f03d8a610efdaa140934c7d3f9e5c0ef6746eede
Johannes Link <business@johanneslink.net>
2020-07-14 16:37:13 +0200

    Refactoring FlatMappedShrinkableTests

--- engine/src/test/java/net/jqwik/engine/properties/shrinking/FlatMappedShrinkableTests.java
+++ engine/src/test/java/net/jqwik/engine/properties/shrinking/FlatMappedShrinkableTests.java
@@ -1,7 +1,6 @@
 package net.jqwik.engine.properties.shrinking;
 
 import java.math.*;
-import java.util.concurrent.atomic.*;
 import java.util.function.*;
 
 import org.mockito.*;
@@ -18,12 +17,8 @@ import static org.mockito.Mockito.*;
 @Label("FlatMappedShrinkable")
 class FlatMappedShrinkableTests {
 
-	private final AtomicInteger counter = new AtomicInteger(0);
-	private final Runnable count = counter::incrementAndGet;
-
 	@SuppressWarnings("unchecked")
 	private final Consumer<String> valueReporter = mock(Consumer.class);
-	private final Consumer<FalsificationResult<String>> reporter = result -> valueReporter.accept(result.value());
 
 	@Example
 	void creation(@ForAll long seed) {
@@ -41,32 +36,20 @@ class FlatMappedShrinkableTests {
 
 		//noinspection unchecked
 		Mockito.reset(valueReporter);
-		counter.set(0);
 
 		Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(4);
 		Function<Integer, Arbitrary<String>> flatMapper = anInt -> Arbitraries.strings().withCharRange('a', 'z').ofLength(anInt);
 		Shrinkable<String> shrinkable = integerShrinkable.flatMap(flatMapper, 1000, seed);
 
-		ShrinkingSequence<String> sequence = shrinkable.shrink(ignore -> TryExecutionResult.falsified(null));
+		Falsifier<String> falsifier = ignore -> TryExecutionResult.falsified(null);
+		String shrunkValue = ShrinkingTestHelper.shrinkToEnd(shrinkable, falsifier, valueReporter, null);
 
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).hasSize(3);
 		verify(valueReporter).accept(ArgumentMatchers.argThat(aString -> aString.length() == 3));
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).hasSize(2);
 		verify(valueReporter).accept(ArgumentMatchers.argThat(aString -> aString.length() == 2));
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-		assertThat(sequence.current().value()).hasSize(1);
 		verify(valueReporter).accept(ArgumentMatchers.argThat(aString -> aString.length() == 1));
-
-		assertThat(sequence.next(count, reporter)).isTrue();
-
-		assertThat(sequence.current().value()).hasSize(0);
 		verify(valueReporter).accept(ArgumentMatchers.argThat(aString -> aString.length() == 0));
 
-		assertThat(counter.get()).isEqualTo(4);
+		assertThat(shrunkValue).isEqualTo("");
 	}
 
 	@Example
@@ -76,11 +59,9 @@ class FlatMappedShrinkableTests {
 		Shrinkable<String> shrinkable = integerShrinkable.flatMap(flatMapper, 1000, seed);
 		assertThat(shrinkable.value()).hasSize(4);
 
-		ShrinkingSequence<String> sequence = shrinkable.shrink((TestingFalsifier<String>) aString -> aString.length() < 3);
-
-		while(sequence.next(count, reporter));
-
-		assertThat(sequence.current().value()).isEqualTo("aaa");
+		TestingFalsifier<String> falsifier = aString -> aString.length() < 3;
+		String shrunkValue = ShrinkingTestHelper.shrinkToEnd(shrinkable, falsifier, null);
+		assertThat(shrunkValue).isEqualTo("aaa");
 	}
 
 	@Example
@@ -95,11 +76,9 @@ class FlatMappedShrinkableTests {
 		Shrinkable<String> shrinkable = integerShrinkable.flatMap(flatMapper, 1000, 42L);
 		assertThat(shrinkable.value()).hasSize(5);
 
-		ShrinkingSequence<String> sequence = shrinkable.shrink((TestingFalsifier<String>) aString -> aString.length() < 3);
-
-		while(sequence.next(count, reporter));
-
-		assertThat(sequence.current().value()).isEqualTo("aaa");
+		TestingFalsifier<String> falsifier = aString -> aString.length() < 3;
+		String shrunkValue = ShrinkingTestHelper.shrinkToEnd(shrinkable, falsifier, null);
+		assertThat(shrunkValue).isEqualTo("aaa");
 	}
 
 }
