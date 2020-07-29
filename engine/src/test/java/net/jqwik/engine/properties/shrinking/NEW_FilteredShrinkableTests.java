cd8a3184b0add4db84e6b30b7b20cf2fd03a2196
Johannes Link <business@johanneslink.net>
2020-07-29 08:53:58 +0200

    Started work on new FilteredShrinkable shrinking. Tests failing.

---
+++ engine/src/test/java/net/jqwik/engine/properties/shrinking/NEW_FilteredShrinkableTests.java
@@ -0,0 +1,53 @@
+package net.jqwik.engine.properties.shrinking;
+
+import net.jqwik.api.*;
+import net.jqwik.engine.properties.shrinking.ShrinkableTypesForTest.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+import static net.jqwik.api.NEW_ShrinkingTestHelper.*;
+
+@Group
+@Label("FilteredShrinkable")
+class NEW_FilteredShrinkableTests {
+
+	@Example
+	void creation() {
+		Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(3);
+		Shrinkable<Integer> shrinkable = integerShrinkable.filter(i -> i % 2 == 1);
+		assertThat(shrinkable.distance()).isEqualTo(ShrinkingDistance.of(3));
+		assertThat(shrinkable.createValue()).isEqualTo(3);
+	}
+
+	@Group
+	class Shrinking {
+
+		@Example
+		void noStepShrinking() {
+			Shrinkable<Integer> integerShrinkable = new FullShrinkable(3);
+			Shrinkable<Integer> shrinkable = integerShrinkable.filter(i -> i % 2 == 1);
+
+			Integer shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), null);
+			assertThat(shrunkValue).isEqualTo(1);
+		}
+
+		@Example
+		void singleStepShrinking() {
+			Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(3);
+			Shrinkable<Integer> shrinkable = integerShrinkable.filter(i -> i % 2 == 1);
+
+			Integer shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), null);
+			assertThat(shrunkValue).isEqualTo(1);
+		}
+
+		@Example
+		void manyStepsShrinking() {
+			Shrinkable<Integer> integerShrinkable = new OneStepShrinkable(41);
+			Shrinkable<Integer> shrinkable = integerShrinkable.filter(i -> i % 2 == 1);
+
+			Integer shrunkValue = shrinkToEnd(shrinkable, alwaysFalsify(), null);
+			assertThat(shrunkValue).isEqualTo(1);
+		}
+	}
+
+}
