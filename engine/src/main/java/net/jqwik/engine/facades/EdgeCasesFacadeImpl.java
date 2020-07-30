4516ba0a34fd092fd2d1ead60b2a225f55fef021
Johannes Link <business@johanneslink.net>
2020-07-30 08:49:24 +0200

    Edge cases of flat mapped shrinkable are now generated lazily

--- engine/src/main/java/net/jqwik/engine/facades/EdgeCasesFacadeImpl.java
+++ engine/src/main/java/net/jqwik/engine/facades/EdgeCasesFacadeImpl.java
@@ -72,10 +72,10 @@ public class EdgeCasesFacadeImpl extends EdgeCases.EdgeCasesFacade {
 								 .map(uSupplier -> {
 									 Function<T, Shrinkable<U>> shrinkableMapper =
 										 newT -> mapper.apply(newT).generator(1000).next(SourceOfRandomness.newRandom(42L));
-									 return (Supplier<Shrinkable<U>>) () -> new FlatMappedEdgeCase<>(
+									 return (Supplier<Shrinkable<U>>) () -> new FixedValueFlatMappedShrinkable<>(
 										 tSupplier.get(),
 										 shrinkableMapper,
-										 uSupplier.get()
+										 uSupplier
 									 );
 								 });
 				})
