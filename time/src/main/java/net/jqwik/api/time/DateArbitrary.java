a9ef324700ec5f5623f9d623acfc6586262c45be
zinki97 <admin@johannes-games.de>
2020-12-10 14:23:19 +0100

    First changes

--- time/src/main/java/net/jqwik/api/time/DateArbitrary.java
+++ time/src/main/java/net/jqwik/api/time/DateArbitrary.java
@@ -5,4 +5,22 @@ import java.time.*;
 import net.jqwik.api.*;
 
 public interface DateArbitrary extends Arbitrary<LocalDate> {
+
+	default DateArbitrary between(LocalDate dateBegin, LocalDate dateEnd){
+		return atTheEarliest(dateBegin).atTheLatest(dateEnd);
+	}
+	DateArbitrary atTheEarliest(LocalDate date);
+	DateArbitrary atTheLatest(LocalDate date);
+
+	DateArbitrary yearGreaterOrEqual(int min);
+	DateArbitrary yearLessOrEqual(int max);
+
+	DateArbitrary monthGreaterOrEqual(int min);
+	DateArbitrary monthLessOrEqual(int max);
+
+	DateArbitrary dayOfMonthGreaterOrEqual(int min);
+	DateArbitrary dayOfMonthLessOrEqual(int max);
+
+	DateArbitrary onlyDaysOfWeek(DayOfWeek... daysOfWeek);
+
 }
