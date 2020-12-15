920ed41f52c9ec38244362d8219488e814b7165a
Johannes Link <business@johanneslink.net>
2020-12-15 16:35:09 +0100

    Added TestingFalsifier to api.testing

---
+++ api/src/main/java/net/jqwik/api/testing/TestingFalsifier.java
@@ -0,0 +1,28 @@
+package net.jqwik.api.testing;
+
+import java.util.function.*;
+
+import org.apiguardian.api.*;
+import org.opentest4j.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.lifecycle.*;
+
+import static org.apiguardian.api.API.Status.*;
+
+@API(status = EXPERIMENTAL, since = "1.4.0")
+public interface TestingFalsifier<T> extends Falsifier<T>, Predicate<T> {
+
+	@Override
+	default TryExecutionResult execute(T parameters) {
+		try {
+			boolean result = this.test(parameters);
+			return result ? TryExecutionResult.satisfied() : TryExecutionResult.falsified(null);
+		} catch (TestAbortedException tea) {
+			return TryExecutionResult.invalid();
+		} catch (AssertionError | Exception e) {
+			return TryExecutionResult.falsified(e);
+		}
+	}
+
+}
