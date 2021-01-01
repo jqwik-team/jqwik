7c52fc4581f58ccf6b58463bc69a65eb0196b1ae
Johannes Link <business@johanneslink.net>
2021-01-01 15:15:25 +0100

    Extracted MappedEdgeCasesConsumer from DefaultIntegerArbitrary

--- engine/src/main/java/net/jqwik/engine/properties/arbitraries/DefaultIntegerArbitrary.java
+++ engine/src/main/java/net/jqwik/engine/properties/arbitraries/DefaultIntegerArbitrary.java
@@ -12,7 +12,7 @@ public class DefaultIntegerArbitrary extends AbstractArbitraryBase implements In
 	private static final int DEFAULT_MIN = Integer.MIN_VALUE;
 	private static final int DEFAULT_MAX = Integer.MAX_VALUE;
 
-	private final IntegralGeneratingArbitrary generatingArbitrary;
+	private IntegralGeneratingArbitrary generatingArbitrary;
 
 	public DefaultIntegerArbitrary() {
 		this.generatingArbitrary = new IntegralGeneratingArbitrary(BigInteger.valueOf(DEFAULT_MIN), BigInteger.valueOf(DEFAULT_MAX));
@@ -35,40 +35,14 @@ public class DefaultIntegerArbitrary extends AbstractArbitraryBase implements In
 
 	@Override
 	public Arbitrary<Integer> edgeCases(Consumer<EdgeCases.Config<Integer>> configurator) {
-		// TODO: Generalize for all number arbitraries
-		Consumer<EdgeCases.Config<BigInteger>> integralConfigurator = bigIntegerConfig -> {
-			EdgeCases.Config<Integer> integerConfig = new EdgeCases.Config<Integer>() {
-				@Override
-				public EdgeCases.Config<Integer> none() {
-					bigIntegerConfig.none();
-					return this;
-				}
-
-				@Override
-				public EdgeCases.Config<Integer> filter(Predicate<Integer> filter) {
-					bigIntegerConfig.filter(bigInteger -> filter.test(bigInteger.intValueExact()));
-					return this;
-				}
-
-				@Override
-				public EdgeCases.Config<Integer> add(Integer edgeCase) {
-					bigIntegerConfig.add(BigInteger.valueOf(edgeCase));
-					return this;
-				}
-
-				@Override
-				public EdgeCases.Config<Integer> includeOnly(Integer... includedValues) {
-					BigInteger[] includedBigIntegers = new BigInteger[includedValues.length];
-					for (int i = 0; i < includedValues.length; i++) {
-						includedBigIntegers[i] = BigInteger.valueOf(includedValues[i]);
-					}
-					bigIntegerConfig.includeOnly(includedBigIntegers);
-					return this;
-				}
-			};
-			configurator.accept(integerConfig);
-		};
-		return generatingArbitrary.edgeCases(integralConfigurator).map(BigInteger::intValueExact);
+		Consumer<EdgeCases.Config<BigInteger>> integralConfigurator = new MappedEdgeCasesConsumer<>(
+				configurator,
+				BigInteger::intValueExact,
+				(Function<Integer, BigInteger>) BigInteger::valueOf
+		);
+		DefaultIntegerArbitrary clone = typedClone();
+		clone.generatingArbitrary = (IntegralGeneratingArbitrary) generatingArbitrary.edgeCases(integralConfigurator);
+		return clone;
 	}
 
 	@Override
---
+++ engine/src/main/java/net/jqwik/engine/properties/arbitraries/MappedEdgeCasesConsumer.java
@@ -0,0 +1,58 @@
+package net.jqwik.engine.properties.arbitraries;
+
+import java.util.function.*;
+
+import net.jqwik.api.*;
+
+class MappedEdgeCasesConsumer<T, U> implements Consumer<EdgeCases.Config<U>> {
+
+	private final Consumer<EdgeCases.Config<T>> tConfigurator;
+	private final Function<U, T> utMapper;
+	private final Function<T, U> tuMapper;
+
+	MappedEdgeCasesConsumer(
+			Consumer<EdgeCases.Config<T>> tConfigurator,
+			Function<U, T> utMapper,
+			Function<T, U> tuMapper
+	) {
+		this.tConfigurator = tConfigurator;
+		this.utMapper = utMapper;
+		this.tuMapper = tuMapper;
+	}
+
+	@Override
+	public void accept(EdgeCases.Config<U> uConfig) {
+
+		EdgeCases.Config<T> tConfig = new EdgeCases.Config<T>() {
+			@Override
+			public EdgeCases.Config<T> none() {
+				uConfig.none();
+				return this;
+			}
+
+			@Override
+			public EdgeCases.Config<T> filter(Predicate<T> filter) {
+				uConfig.filter(u -> filter.test(utMapper.apply(u)));
+				return this;
+			}
+
+			@Override
+			public EdgeCases.Config<T> add(T edgeCase) {
+				uConfig.add(tuMapper.apply(edgeCase));
+				return this;
+			}
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public EdgeCases.Config<T> includeOnly(T... includedValues) {
+				Object[] includedBigIntegers = new Object[includedValues.length];
+				for (int i = 0; i < includedValues.length; i++) {
+					includedBigIntegers[i] = tuMapper.apply(includedValues[i]);
+				}
+				uConfig.includeOnly((U[]) includedBigIntegers);
+				return this;
+			}
+		};
+		tConfigurator.accept(tConfig);
+	}
+}
