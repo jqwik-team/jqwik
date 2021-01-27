cf93af97c11241ccce2537bd869cb49d12e2a773
zinki97 <admin@johannes-games.de>
2021-01-27 12:33:24 +0100

    first commit

---
+++ time/src/main/java/net/jqwik/time/api/Times.java
@@ -0,0 +1,14 @@
+package net.jqwik.time.api;
+
+import org.apiguardian.api.*;
+
+import static org.apiguardian.api.API.Status.*;
+
+@API(status = EXPERIMENTAL, since = "1.4.1")
+public class Times {
+
+	private Times() {
+		// Must never be called
+	}
+
+}
