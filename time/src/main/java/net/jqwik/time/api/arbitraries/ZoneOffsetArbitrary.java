92fe1710737b0f85825cc75fa47eb8ec001844a3
zinki97 <admin@johannes-games.de>
2021-02-11 16:10:32 +0100

    new algorithm to calculate ZoneOffset

--- time/src/main/java/net/jqwik/time/api/arbitraries/ZoneOffsetArbitrary.java
+++ time/src/main/java/net/jqwik/time/api/arbitraries/ZoneOffsetArbitrary.java
@@ -14,4 +14,42 @@ import static org.apiguardian.api.API.Status.*;
 @API(status = EXPERIMENTAL, since = "1.4.1")
 public interface ZoneOffsetArbitrary extends Arbitrary<ZoneOffset> {
 
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated zone offset values.
+	 */
+	default ZoneOffsetArbitrary between(ZoneOffset min, ZoneOffset max) {
+		if (min.getTotalSeconds() > max.getTotalSeconds()) {
+			return atTheEarliest(max).atTheLatest(min);
+		}
+		return atTheEarliest(min).atTheLatest(max);
+	}
+
+	/**
+	 * Set the allowed lower {@code min} (included) bounder of generated zone offset values.
+	 */
+	ZoneOffsetArbitrary atTheEarliest(ZoneOffset min);
+
+	/**
+	 * Set the allowed upper {@code max} (included) bounder of generated zone offset values.
+	 */
+	ZoneOffsetArbitrary atTheLatest(ZoneOffset max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated hour values.
+	 * The hours can be between {@code -18} and {@code 18}.
+	 */
+	ZoneOffsetArbitrary hourBetween(int min, int max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated minute values.
+	 * The minutes can be between {@code 0} and {@code 59}.
+	 */
+	ZoneOffsetArbitrary minuteBetween(int min, int max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated second values.
+	 * The minutes can be between {@code 0} and {@code 59}.
+	 */
+	ZoneOffsetArbitrary secondBetween(int min, int max);
+
 }
--- time/src/main/java/net/jqwik/time/internal/properties/arbitraries/DefaultZoneOffsetArbitrary.java
+++ time/src/main/java/net/jqwik/time/internal/properties/arbitraries/DefaultZoneOffsetArbitrary.java
@@ -13,8 +13,11 @@ import static org.apiguardian.api.API.Status.*;
 @API(status = INTERNAL)
 public class DefaultZoneOffsetArbitrary extends ArbitraryDecorator<ZoneOffset> implements ZoneOffsetArbitrary {
 
-	private int hourMin = -17; //TODO: -18
-	private int hourMax = 17; //TODO: 18
+	private ZoneOffset offsetMin = ZoneOffset.MIN;
+	private ZoneOffset offsetMax = ZoneOffset.MAX;
+
+	private int hourMin = -18;
+	private int hourMax = 18;
 	private int hourPeriod;
 
 	private int minuteMin = 0;
@@ -30,32 +33,117 @@ public class DefaultZoneOffsetArbitrary extends ArbitraryDecorator<ZoneOffset> i
 	@Override
 	protected Arbitrary<ZoneOffset> arbitrary() {
 
+		/*if (hourMin == hourMax && hourMin == -18) {
+			if (minuteMin != 0 || secondMin != 0) {
+				throw new IllegalArgumentException("Minutes and Seconds must be 0 because hour value is -18.");
+			} else if (offsetMin.getTotalSeconds() != ZoneOffset.MIN.getTotalSeconds()) {
+				throw new IllegalArgumentException("Hour value can only be -18, but minimum zone offset value is not ZoneOffset.MIN");
+			}
+			return Arbitraries.just(ZoneOffset.MIN);
+		} else if (hourMin == hourMax && hourMax == 18) {
+			if (minuteMin != 0 || secondMin != 0) {
+				throw new IllegalArgumentException("Minutes and Seconds must be 0 because hour value is 18.");
+			} else if (offsetMax.getTotalSeconds() != ZoneOffset.MAX.getTotalSeconds()) {
+				throw new IllegalArgumentException("Hour value can only be 18, but maximum zone offset value is not ZoneOffset.MAX");
+			}
+			return Arbitraries.just(ZoneOffset.MAX);
+		}
+
 		int secondsBetween = calculateSecondsBetween();
 
 		Arbitrary<Integer> seconds = Arbitraries.integers()
 												.withDistribution(RandomDistribution.uniform())
 												.between(0, secondsBetween)
-												.edgeCases(edgeCases -> {
-													edgeCases.includeOnly(0, secondsBetween);
-												});
+												.edgeCases(EdgeCases.Config::none);
 
-		Arbitrary<ZoneOffset> zoneOffsets = seconds.map(this::calculateZoneOffset);
+		Arbitrary<ZoneOffset> zoneOffsets = seconds.map(this::calculateZoneOffset)
+												   .filter(v -> v.getTotalSeconds() >= offsetMin
+																							   .getTotalSeconds() && v.getTotalSeconds() <= offsetMax
+																																					.getTotalSeconds())
+												   .edgeCases(edgeCases -> {
+													   edgeCases.add(offsetMin, offsetMax);
+													   edgeCases.includeOnly(offsetMin, offsetMax);
+												   });*/
+
+		if (hourMin == hourMax && hourMin == -18) {
+			if (minuteMin != 0 || secondMin != 0) {
+				throw new IllegalArgumentException("Minutes and Seconds must be 0 because hour value is -18.");
+			} else if (offsetMin.getTotalSeconds() != ZoneOffset.MIN.getTotalSeconds()) {
+				throw new IllegalArgumentException("Hour value can only be -18, but minimum zone offset value is not ZoneOffset.MIN");
+			}
+			return Arbitraries.just(ZoneOffset.MIN);
+		} else if (hourMin == hourMax && hourMax == 18) {
+			if (minuteMin != 0 || secondMin != 0) {
+				throw new IllegalArgumentException("Minutes and Seconds must be 0 because hour value is 18.");
+			} else if (offsetMax.getTotalSeconds() != ZoneOffset.MAX.getTotalSeconds()) {
+				throw new IllegalArgumentException("Hour value can only be 18, but maximum zone offset value is not ZoneOffset.MAX");
+			}
+			return Arbitraries.just(ZoneOffset.MAX);
+		}
+
+		Arbitrary<Integer> seconds = Arbitraries.integers()
+												.withDistribution(RandomDistribution.uniform())
+												.between(offsetMin.getTotalSeconds(), offsetMax.getTotalSeconds());
+
+		Arbitrary<ZoneOffset> zoneOffsets = seconds.map(ZoneOffset::ofTotalSeconds)
+												   .filter(this::filterOffsets);
 
 		return zoneOffsets;
 
 	}
 
+	private boolean filterOffsets(ZoneOffset offset) {
+
+		int hour = calculateHourValue(offset);
+		int minute = calculateMinuteValue(offset);
+		int second = calculateSecondValue(offset);
+
+		return hour >= hourMin && hour <= hourMax && minute >= minuteMin && minute <= minuteMax && second >= secondMin && second <= secondMax;
+
+	}
+
+	private int calculateHourValue(ZoneOffset offset) {
+		return offset.getTotalSeconds() / 3600;
+	}
+
+	private int calculateMinuteValue(ZoneOffset offset) {
+		return Math.abs((offset.getTotalSeconds() % 3600) / 60);
+	}
+
+	private int calculateSecondValue(ZoneOffset offset) {
+		return Math.abs(offset.getTotalSeconds() % 60);
+	}
+
+	/*private void calculateEffectiveValues() {
+		hourMin = offsetMin.getTotalSeconds()/3600;
+		hourMax = offsetMax.getTotalSeconds()/3600;
+		if (hourMin == hourMax) {
+			minuteMin = Math.abs((offsetMin.getTotalSeconds()%3600)/60);
+			minuteMax = Math.abs((offsetMax.getTotalSeconds()%3600)/60);
+			if (minuteMin == minuteMax) {
+				secondMin = Math.abs(offsetMin.getTotalSeconds()%60);
+				secondMax = Math.abs(offsetMax.getTotalSeconds()%60);
+			}
+		}
+	}
+
 	private ZoneOffset calculateZoneOffset(int val) {
 
-		//TODO: -00:xx:xx is not possible at the moment
-		//TODO: -00:00:xx is not possible at the moment
-
 		int hours = hourMin;
+		if (hours == -18) {
+			hours++;
+		}
 		int minutes = minuteMin;
 		int seconds = secondMin;
+		boolean negativeZero = false;
 
 		int hoursAdd = val / helperMinuteSecond;
 		hours += hoursAdd;
+		int effectiveHourMax = hourMax != 18 ? hourMax : hourMax - 1;
+		if (hours > effectiveHourMax) {
+			hours = 0;
+			negativeZero = true;
+		}
 
 		val -= hoursAdd * helperMinuteSecond;
 
@@ -65,10 +153,10 @@ public class DefaultZoneOffsetArbitrary extends ArbitraryDecorator<ZoneOffset> i
 		int secondsAdd = val - (minutesAdd * secondPeriod);
 		seconds += secondsAdd;
 
-		if (hours < 0) {
+		if (hours < 0 || negativeZero) {
 			minutes *= -1;
 		}
-		if (minutes < 0 || hours < 0) {
+		if (minutes < 0 || hours < 0 || negativeZero) {
 			seconds *= -1;
 		}
 
@@ -82,10 +170,96 @@ public class DefaultZoneOffsetArbitrary extends ArbitraryDecorator<ZoneOffset> i
 		minutePeriod = 1 + minuteMax - minuteMin;
 		secondPeriod = 1 + secondMax - secondMin;
 
+		if (hourMin == -18) {
+			hourPeriod--;
+		}
+		if (hourMax == 18) {
+			hourPeriod--;
+		}
+
+		if (hourMin <= 0 && hourMax >= 0) {
+			hourPeriod++;
+		}
+
 		helperMinuteSecond = minutePeriod * secondPeriod;
 
 		return (hourPeriod * helperMinuteSecond) - 1;
 
+	}*/
+
+	@Override
+	public ZoneOffsetArbitrary atTheEarliest(ZoneOffset min) {
+		if ((offsetMax != null) && min.getTotalSeconds() > offsetMax.getTotalSeconds()) {
+			throw new IllegalArgumentException("Minimum offset must not be after maximum offset");
+		}
+
+		DefaultZoneOffsetArbitrary clone = typedClone();
+		clone.offsetMin = min;
+		return clone;
 	}
 
+	@Override
+	public ZoneOffsetArbitrary atTheLatest(ZoneOffset max) {
+		if ((offsetMin != null) && max.getTotalSeconds() < offsetMin.getTotalSeconds()) {
+			throw new IllegalArgumentException("Maximum time must not be before minimum time");
+		}
+
+		DefaultZoneOffsetArbitrary clone = typedClone();
+		clone.offsetMax = max;
+		return clone;
+	}
+
+	@Override
+	public ZoneOffsetArbitrary hourBetween(int min, int max) {
+		if (min > max) {
+			int remember = min;
+			min = max;
+			max = remember;
+		}
+
+		if (min < -18 || max > 18) {
+			throw new IllegalArgumentException("Hour value must be between -18 and 18.");
+		}
+
+		DefaultZoneOffsetArbitrary clone = typedClone();
+		clone.hourMin = min;
+		clone.hourMax = max;
+		return clone;
+	}
+
+	@Override
+	public ZoneOffsetArbitrary minuteBetween(int min, int max) {
+		if (min > max) {
+			int remember = min;
+			min = max;
+			max = remember;
+		}
+
+		if (min < 0 || max > 59) {
+			throw new IllegalArgumentException("Minute value must be between 0 and 59.");
+		}
+
+		DefaultZoneOffsetArbitrary clone = typedClone();
+		clone.minuteMin = min;
+		clone.minuteMax = max;
+		return clone;
+	}
+
+	@Override
+	public ZoneOffsetArbitrary secondBetween(int min, int max) {
+		if (min > max) {
+			int remember = min;
+			min = max;
+			max = remember;
+		}
+
+		if (min < 0 || max > 59) {
+			throw new IllegalArgumentException("Second value must be between 0 and 59.");
+		}
+
+		DefaultZoneOffsetArbitrary clone = typedClone();
+		clone.secondMin = min;
+		clone.secondMax = max;
+		return clone;
+	}
 }
