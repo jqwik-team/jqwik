7e73ce2dbf9deb679447bacec9558c94dae48762
zinki97 <admin@johannes-games.de>
2021-02-18 15:37:16 +0100

    Added tests for worst cases and fixed them in DurationArbitrary

--- time/src/test/java/net/jqwik/time/api/DurationTests.java
+++ time/src/test/java/net/jqwik/time/api/DurationTests.java
@@ -4,8 +4,10 @@ import java.time.*;
 import java.util.*;
 
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
 import net.jqwik.testing.*;
 import net.jqwik.time.api.arbitraries.*;
+import net.jqwik.time.internal.properties.arbitraries.*;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -26,6 +28,196 @@ class DurationTests {
 		@Property
 		void validDurationIsGenerated(@ForAll("durations") Duration duration) {
 			assertThat(duration).isNotNull();
+		}
+
+		@Group
+		class WorstCases {
+
+			@Property
+			void worstCaseGeneration(
+					@ForAll("worstCase") Duration duration,
+					@ForAll @IntRange(min = 999_999_800, max = 999_999_999) int nanosStart,
+					@ForAll @IntRange(min = 0, max = 200) int nanosEnd,
+					@ForAll Random random
+			) {
+
+				Duration start = duration.withNanos(nanosStart);
+				Duration end = Duration.ofSeconds(start.getSeconds() + 1, nanosEnd);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration2(
+					@ForAll("worstCase") Duration duration,
+					@ForAll @IntRange(min = 999_999_800, max = 999_999_999) int nanosStart,
+					@ForAll @IntRange(min = 0, max = 200) int nanosEnd,
+					@ForAll Random random
+			) {
+
+				Duration start = duration.withNanos(nanosStart);
+				Duration end = Duration.ofSeconds(start.getSeconds() + 2, nanosEnd);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration3(
+					@ForAll("worstCaseNegative") Duration duration,
+					@ForAll @IntRange(min = 999_999_800, max = 999_999_999) int nanosEnd,
+					@ForAll @IntRange(min = 0, max = 200) int nanosStart,
+					@ForAll Random random
+			) {
+
+				Duration start = duration.withNanos(nanosEnd);
+				Duration end = Duration.ofSeconds(start.getSeconds() + 1, nanosStart);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration4(
+					@ForAll("worstCaseNegative") Duration duration,
+					@ForAll @IntRange(min = 999_999_800, max = 999_999_999) int nanosEnd,
+					@ForAll @IntRange(min = 0, max = 200) int nanosStart,
+					@ForAll Random random
+			) {
+
+				Duration start = duration.withNanos(nanosEnd);
+				Duration end = Duration.ofSeconds(start.getSeconds() + 2, nanosStart);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration5(
+					@ForAll("worstCase") Duration start,
+					@ForAll @IntRange(min = 0, max = 200) int nanosAdd,
+					@ForAll Random random
+			) {
+
+				Assume.that(start.getNano() < 1_000_000_000 - nanosAdd);
+
+				Duration end = Duration.ofSeconds(start.getSeconds() + 1, start.getNano() + nanosAdd);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration6(
+					@ForAll("worstCase") Duration start,
+					@ForAll @IntRange(min = 0, max = 200) int nanosAdd,
+					@ForAll Random random
+			) {
+
+				Assume.that(start.getNano() < 1_000_000_000 - nanosAdd);
+
+				Duration end = Duration.ofSeconds(start.getSeconds() + 2, start.getNano() + nanosAdd);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration7(
+					@ForAll("worstCaseNegative") Duration start,
+					@ForAll @IntRange(min = 0, max = 200) int nanosSubtract,
+					@ForAll Random random
+			) {
+
+				Assume.that(start.getNano() > nanosSubtract);
+
+				Duration end = Duration.ofSeconds(start.getSeconds() + 1, start.getNano() - nanosSubtract);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration8(
+					@ForAll("worstCaseNegative") Duration start,
+					@ForAll @IntRange(min = 0, max = 200) int nanosSubtract,
+					@ForAll Random random
+			) {
+
+				Assume.that(start.getNano() > nanosSubtract);
+
+				Duration end = Duration.ofSeconds(start.getSeconds() + 2, start.getNano() - nanosSubtract);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Property
+			void worstCaseGeneration9(@ForAll Random random) {
+
+				Duration start = Duration.ofSeconds(0, -101);
+				Duration end = Duration.ofSeconds(0, 100);
+
+				Arbitrary<Duration> durations = Times.durations().between(start, end);
+
+				assertAllGenerated(durations.generator(1000), random, durationGenerated -> {
+					assertThat(durationGenerated).isNotNull();
+					return true;
+				});
+
+			}
+
+			@Provide
+			Arbitrary<Duration> worstCase() {
+				return Times.durations().between(Duration.ZERO, Duration.ofSeconds(Long.MAX_VALUE - 2, 999_999_999));
+			}
+
+			@Provide
+			Arbitrary<Duration> worstCaseNegative() {
+				return Times.durations().between(DefaultDurationArbitrary.DEFAULT_MIN, Duration.ofSeconds(-2, 0));
+			}
+
 		}
 
 	}
