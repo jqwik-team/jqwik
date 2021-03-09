e36fe717cefbd3887354aadc873dfcd8efb77289
zinki97 <admin@johannes-games.de>
2021-03-09 13:38:56 +0100

    Added class DateTimes

---
+++ time/src/main/java/net/jqwik/time/api/DateTimes.java
@@ -0,0 +1,14 @@
+package net.jqwik.time.api;
+
+import org.apiguardian.api.*;
+
+import static org.apiguardian.api.API.Status.*;
+
+@API(status = EXPERIMENTAL, since = "1.5.2")
+public class DateTimes {
+
+	private DateTimes() {
+		// Must never be called
+	}
+
+}
