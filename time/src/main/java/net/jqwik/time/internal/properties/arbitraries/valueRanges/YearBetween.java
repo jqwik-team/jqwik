1ca33fad018a97c84ccb7eb0038695d4b3460518
zinki97 <admin@johannes-games.de>
2021-04-06 16:50:48 +0200

    DefaultCalendarArbitrary and DefaultDateArbitrary with ValueRanges

--- time/src/main/java/net/jqwik/time/internal/properties/arbitraries/valueRanges/YearBetween.java
+++ time/src/main/java/net/jqwik/time/internal/properties/arbitraries/valueRanges/YearBetween.java
@@ -4,6 +4,16 @@ import java.time.*;
 
 public class YearBetween extends ValueRange<Year> {
 
+	private boolean useInCalendar;
+
+	public YearBetween() {
+		this(false);
+	}
+
+	public YearBetween(boolean useInCalendar) {
+		this.useInCalendar = useInCalendar;
+	}
+
 	@Override
 	protected void minMaxChanger(Parameter parameter) {
 		if (parameter.getMin().isAfter(parameter.getMax())) {
@@ -13,8 +23,17 @@ public class YearBetween extends ValueRange<Year> {
 
 	@Override
 	protected void exceptionCheck(Parameter parameter) {
-		if (parameter.getMin().getValue() <= 0 || parameter.getMax().getValue() <= 0) {
+		if (parameter.getMin() != null && parameter.getMin().getValue() <= 0) {
 			throw new IllegalArgumentException("Minimum year must be > 0");
+		}
+		if (parameter.getMax() != null && parameter.getMax().getValue() <= 0) {
+			throw new IllegalArgumentException("Maximum year must be > 0");
+		}
+		if (useInCalendar && parameter.getMin() != null && parameter.getMin().getValue() > 292_278_993) {
+			throw new IllegalArgumentException("Minimum year in a calendar based date must be <= 292278993");
+		}
+		if (useInCalendar && parameter.getMax() != null && parameter.getMax().getValue() > 292_278_993) {
+			throw new IllegalArgumentException("Maximum year in a calendar based date must be <= 292278993");
 		}
 	}
 }
