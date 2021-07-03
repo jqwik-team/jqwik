10bec450c7de3c08b89e9c91e131b8ca3ccad20d
Johannes Link <business@johanneslink.net>
2021-07-03 21:53:36 +0200

    Added test for largish set generation

--- engine/src/test/java/net/jqwik/api/SetArbitraryTests.java
+++ engine/src/test/java/net/jqwik/api/SetArbitraryTests.java
@@ -10,8 +10,9 @@ import net.jqwik.api.edgeCases.*;
 import static java.util.Arrays.*;
 import static org.assertj.core.api.Assertions.*;
 
-import static net.jqwik.api.ArbitraryTestHelper.*;
+import static net.jqwik.api.ArbitraryTestHelper.assertAllGenerated;
 import static net.jqwik.testing.ShrinkingSupport.*;
+import static net.jqwik.testing.TestingSupport.assertAllGenerated;
 import static net.jqwik.testing.TestingSupport.*;
 
 @Group
@@ -28,6 +29,20 @@ class SetArbitraryTests {
 	}
 
 	@Example
+	void largeSetOfFixedSize(@ForAll Random random) {
+		int size = 1000;
+		Arbitrary<Integer> integerArbitrary = Arbitraries.integers();
+		SetArbitrary<Integer> setArbitrary = integerArbitrary.set().ofSize(size);
+
+		RandomGenerator<Set<Integer>> generator = setArbitrary.generator(1000, true);
+
+		for (int i = 0; i < 5; i++) { // assertAllGenerated generates too many samples
+			Set<Integer> set = generator.next(random).value();
+			assertThat(set.size()).isEqualTo(size);
+		}
+	}
+
+	@Example
 	void setWithLessElementsThanMaxSize() {
 		Arbitrary<Integer> integerArbitrary = Arbitraries.of(1, 2, 3, 4, 5);
 		SetArbitrary<Integer> setArbitrary = integerArbitrary.set().ofMinSize(2);
@@ -41,9 +56,9 @@ class SetArbitraryTests {
 	void mapEach() {
 		Arbitrary<Integer> integerArbitrary = Arbitraries.integers().between(1, 10);
 		Arbitrary<Set<Tuple.Tuple2<Integer, Set<Integer>>>> setArbitrary =
-				integerArbitrary
-						.set().ofSize(5)
-						.mapEach((all, each) -> Tuple.of(each, all));
+			integerArbitrary
+				.set().ofSize(5)
+				.mapEach((all, each) -> Tuple.of(each, all));
 
 		RandomGenerator<Set<Tuple.Tuple2<Integer, Set<Integer>>>> generator = setArbitrary.generator(1, true);
 
@@ -57,11 +72,11 @@ class SetArbitraryTests {
 	void flatMapEach() {
 		Arbitrary<Integer> integerArbitrary = Arbitraries.integers().between(1, 10);
 		Arbitrary<Set<Tuple.Tuple2<Integer, Integer>>> setArbitrary =
-				integerArbitrary
-						.set().ofSize(5)
-						.flatMapEach((all, each) ->
-											 Arbitraries.of(all).map(friend -> Tuple.of(each, friend))
-						);
+			integerArbitrary
+				.set().ofSize(5)
+				.flatMapEach((all, each) ->
+								 Arbitraries.of(all).map(friend -> Tuple.of(each, friend))
+				);
 
 		RandomGenerator<Set<Tuple.Tuple2<Integer, Integer>>> generator = setArbitrary.generator(1, true);
 
@@ -74,9 +89,9 @@ class SetArbitraryTests {
 	@Example
 	void multipleUniquenessConstraints(@ForAll Random random) {
 		SetArbitrary<Integer> setArbitrary =
-				Arbitraries.integers().between(1, 1000).set().ofMaxSize(20)
-						   .uniqueElements(i -> i % 99)
-						   .uniqueElements(i -> i % 100);
+			Arbitraries.integers().between(1, 1000).set().ofMaxSize(20)
+					   .uniqueElements(i -> i % 99)
+					   .uniqueElements(i -> i % 100);
 
 		RandomGenerator<Set<Integer>> generator = setArbitrary.generator(1000, true);
 
@@ -96,65 +111,64 @@ class SetArbitraryTests {
 		return new HashSet<>(moduloList).size() == list.size();
 	}
 
-
 	@Group
 	class ExhaustiveGeneration {
 		@Example
 		void setsAreCombinationsOfElementsUpToMaxLength() {
 			Optional<ExhaustiveGenerator<Set<Integer>>> optionalGenerator =
-					Arbitraries.integers().between(1, 3).set().ofMaxSize(2).exhaustive();
+				Arbitraries.integers().between(1, 3).set().ofMaxSize(2).exhaustive();
 			assertThat(optionalGenerator).isPresent();
 
 			ExhaustiveGenerator<Set<Integer>> generator = optionalGenerator.get();
 			assertThat(generator.maxCount()).isEqualTo(7);
 			assertThat(generator).containsExactly(
-					asSet(),
-					asSet(1),
-					asSet(2),
-					asSet(3),
-					asSet(1, 2),
-					asSet(1, 3),
-					asSet(2, 3)
+				asSet(),
+				asSet(1),
+				asSet(2),
+				asSet(3),
+				asSet(1, 2),
+				asSet(1, 3),
+				asSet(2, 3)
 			);
 		}
 
 		@Example
 		void combinationsAreFilteredByUniquenessConstraints() {
 			Optional<ExhaustiveGenerator<Set<Integer>>> optionalGenerator =
-					Arbitraries.integers().between(1, 4).set().ofMaxSize(2).uniqueElements(i -> i % 3)
-							   .exhaustive();
+				Arbitraries.integers().between(1, 4).set().ofMaxSize(2).uniqueElements(i -> i % 3)
+						   .exhaustive();
 
 			assertThat(optionalGenerator).isPresent();
 
 			ExhaustiveGenerator<Set<Integer>> generator = optionalGenerator.get();
 			assertThat(generator.maxCount()).isEqualTo(11);
 			assertThat(generator).containsExactlyInAnyOrder(
-					asSet(),
-					asSet(1),
-					asSet(2),
-					asSet(3),
-					asSet(4),
-					asSet(1, 2),
-					asSet(1, 3),
-					asSet(2, 3),
-					asSet(2, 4),
-					asSet(3, 4)
+				asSet(),
+				asSet(1),
+				asSet(2),
+				asSet(3),
+				asSet(4),
+				asSet(1, 2),
+				asSet(1, 3),
+				asSet(2, 3),
+				asSet(2, 4),
+				asSet(3, 4)
 			);
 		}
 
 		@Example
 		void lessElementsThanSetSize() {
 			Optional<ExhaustiveGenerator<Set<Integer>>> optionalGenerator =
-					Arbitraries.integers().between(1, 2).set().ofMaxSize(5).exhaustive();
+				Arbitraries.integers().between(1, 2).set().ofMaxSize(5).exhaustive();
 			assertThat(optionalGenerator).isPresent();
 
 			ExhaustiveGenerator<Set<Integer>> generator = optionalGenerator.get();
 			assertThat(generator.maxCount()).isEqualTo(4);
 			assertThat(generator).containsExactly(
-					asSet(),
-					asSet(1),
-					asSet(2),
-					asSet(1, 2)
+				asSet(),
+				asSet(1),
+				asSet(2),
+				asSet(1, 2)
 			);
 		}
 
@@ -165,14 +179,14 @@ class SetArbitraryTests {
 		@Example
 		void elementArbitraryNotExhaustive() {
 			Optional<ExhaustiveGenerator<Set<Double>>> optionalGenerator =
-					Arbitraries.doubles().between(1, 10).set().ofMaxSize(1).exhaustive();
+				Arbitraries.doubles().between(1, 10).set().ofMaxSize(1).exhaustive();
 			assertThat(optionalGenerator).isNotPresent();
 		}
 
 		@Example
 		void tooManyCombinations() {
 			Optional<ExhaustiveGenerator<Set<Integer>>> optionalGenerator =
-					Arbitraries.integers().between(1, 75).set().ofMaxSize(10).exhaustive();
+				Arbitraries.integers().between(1, 75).set().ofMaxSize(10).exhaustive();
 			assertThat(optionalGenerator).isNotPresent();
 		}
 	}
@@ -192,16 +206,16 @@ class SetArbitraryTests {
 			IntegerArbitrary ints = Arbitraries.integers().between(-10, 10);
 			Arbitrary<Set<Integer>> arbitrary = ints.set();
 			assertThat(collectEdgeCaseValues(arbitrary.edgeCases())).containsExactlyInAnyOrder(
-					Collections.emptySet(),
-					Collections.singleton(-10),
-					Collections.singleton(-9),
-					Collections.singleton(-2),
-					Collections.singleton(-1),
-					Collections.singleton(0),
-					Collections.singleton(1),
-					Collections.singleton(2),
-					Collections.singleton(9),
-					Collections.singleton(10)
+				Collections.emptySet(),
+				Collections.singleton(-10),
+				Collections.singleton(-9),
+				Collections.singleton(-2),
+				Collections.singleton(-1),
+				Collections.singleton(0),
+				Collections.singleton(1),
+				Collections.singleton(2),
+				Collections.singleton(9),
+				Collections.singleton(10)
 			);
 			assertThat(collectEdgeCaseValues(arbitrary.edgeCases())).hasSize(10);
 		}
@@ -211,15 +225,15 @@ class SetArbitraryTests {
 			IntegerArbitrary ints = Arbitraries.integers().between(-10, 10);
 			Arbitrary<Set<Integer>> arbitrary = ints.set().ofMinSize(1);
 			assertThat(collectEdgeCaseValues(arbitrary.edgeCases())).containsExactlyInAnyOrder(
-					Collections.singleton(-10),
-					Collections.singleton(-9),
-					Collections.singleton(-2),
-					Collections.singleton(-1),
-					Collections.singleton(0),
-					Collections.singleton(1),
-					Collections.singleton(2),
-					Collections.singleton(9),
-					Collections.singleton(10)
+				Collections.singleton(-10),
+				Collections.singleton(-9),
+				Collections.singleton(-2),
+				Collections.singleton(-1),
+				Collections.singleton(0),
+				Collections.singleton(1),
+				Collections.singleton(2),
+				Collections.singleton(9),
+				Collections.singleton(10)
 			);
 		}
 
@@ -255,18 +269,17 @@ class SetArbitraryTests {
 		@Property
 		void shrinkWithUniqueness(@ForAll Random random, @ForAll @IntRange(min = 2, max = 9) int min) {
 			SetArbitrary<Integer> lists =
-					Arbitraries.integers().between(1, 1000).set().ofMinSize(min).ofMaxSize(9)
-							   .uniqueElements(i -> i % 10);
+				Arbitraries.integers().between(1, 1000).set().ofMinSize(min).ofMaxSize(9)
+						   .uniqueElements(i -> i % 10);
 			Set<Integer> value = falsifyThenShrink(lists, random);
 			assertThat(value).hasSize(min);
 			assertThat(isUniqueModulo(value, 10))
-					.describedAs("%s is not unique mod 10", value)
-					.isTrue();
+				.describedAs("%s is not unique mod 10", value)
+				.isTrue();
 			assertThat(value).allMatch(i -> i <= min);
 		}
 
 	}
-
 
 	private void assertGeneratedSet(RandomGenerator<Set<Integer>> generator, int minSize, int maxSize) {
 		assertAllGenerated(generator, set -> {
