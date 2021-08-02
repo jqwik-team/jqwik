7a590ad5082e42025cc612ba4ecc83a8980845df
zinki97 <admin@johannes-games.de>
2021-08-02 16:55:01 +0200

    methods for InstantArbitrary (without tests)

--- time/src/main/java/net/jqwik/time/api/arbitraries/InstantArbitrary.java
+++ time/src/main/java/net/jqwik/time/api/arbitraries/InstantArbitrary.java
@@ -1,6 +1,7 @@
 package net.jqwik.time.api.arbitraries;
 
 import java.time.*;
+import java.time.temporal.*;
 
 import org.apiguardian.api.*;
 
@@ -12,8 +13,129 @@ import static org.apiguardian.api.API.Status.*;
  * Fluent interface to configure the generation of instant values.
  * All generated instants use the Gregorian Calendar, even if they are before October 15, 1582.
  * By default, instants with years between 1900 and 2500 are generated.
+ * Max possible year is 999_999_999.
  */
 @API(status = EXPERIMENTAL, since = "1.5.5")
 public interface InstantArbitrary extends Arbitrary<Instant> {
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated instant values.
+	 * If you don't explicitly set the precision and use min/max values with precision milliseconds/microseconds/nanoseconds, the precision of your min/max value is implicitly set.
+	 */
+	default InstantArbitrary between(Instant min, Instant max) {
+		if (min.isAfter(max)) {
+			return atTheEarliest(max).atTheLatest(min);
+		}
+		return atTheEarliest(min).atTheLatest(max);
+	}
+
+	/**
+	 * Set the allowed lower {@code min} (included) bounder of generated instant values.
+	 * If you don't explicitly set the precision and use min/max values with precision milliseconds/microseconds/nanoseconds, the precision of your min/max value is implicitly set.
+	 */
+	InstantArbitrary atTheEarliest(Instant min);
+
+	/**
+	 * Set the allowed upper {@code max} (included) bounder of generated instant values.
+	 * If you don't explicitly set the precision and use min/max values with precision milliseconds/microseconds/nanoseconds, the precision of your min/max value is implicitly set.
+	 */
+	InstantArbitrary atTheLatest(Instant max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated local date values.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary dateBetween(LocalDate min, LocalDate max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated year values.
+	 * The years can be between {@code 1} and {@code Year.MAX_VALUE}.
+	 *
+	 * <p>Calling this method is equivalent to calling {@linkplain #dateBetween(LocalDate, LocalDate)}
+	 * assuming Jan 1 and Dec 31 as first and last day of those years.</p>
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary yearBetween(Year min, Year max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated year values.
+	 * The {@code int} values can be between {@code 1} and {@code Year.MAX_VALUE}.
+	 *
+	 * <p>Calling this method is equivalent to calling {@linkplain #dateBetween(LocalDate, LocalDate)}
+	 * assuming Jan 1 and Dec 31 as first and last day of those years.</p>
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	default InstantArbitrary yearBetween(int min, int max) {
+		return yearBetween(Year.of(min), Year.of(max));
+	}
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated month values.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary monthBetween(Month min, Month max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated month values.
+	 * The {@code int} values can be between 1 and 12.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	default InstantArbitrary monthBetween(int min, int max) {
+		return monthBetween(Month.of(min), Month.of(max));
+	}
+
+	/**
+	 * Set an array of allowed {@code months}.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary onlyMonths(Month... months);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated day of month values.
+	 * The {@code int} values can be between 1 and 31.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary dayOfMonthBetween(int min, int max);
+
+	/**
+	 * Set an array of allowed {@code daysOfWeek}.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary onlyDaysOfWeek(DayOfWeek... daysOfWeek);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated local time values.
+	 * If you don't explicitly set the precision and use min/max values with precision milliseconds/microseconds/nanoseconds, the precision of your min/max value is implicitly set.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary timeBetween(LocalTime min, LocalTime max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated hour values.
+	 * The hours can be between {@code 0} and {@code 23}.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary hourBetween(int min, int max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated minute values.
+	 * The minutes can be between {@code 0} and {@code 59}.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary minuteBetween(int min, int max);
+
+	/**
+	 * Set the allowed lower {@code min} (included) and upper {@code max} (included) bounder of generated second values.
+	 * The minutes can be between {@code 0} and {@code 59}.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary secondBetween(int min, int max);
+
+	/**
+	 * Constrain the precision of generated values.
+	 * Default value: Seconds. If you don't explicitly set the precision and use min/max values with precision milliseconds/microseconds/nanoseconds, the precision of your min/max value is implicitly set.
+	 */
+	@API(status = EXPERIMENTAL, since = "1.5.2")
+	InstantArbitrary ofPrecision(ChronoUnit ofPrecision);
 
 }
--- time/src/main/java/net/jqwik/time/internal/properties/arbitraries/DefaultInstantArbitrary.java
+++ time/src/main/java/net/jqwik/time/internal/properties/arbitraries/DefaultInstantArbitrary.java
@@ -1,6 +1,7 @@
 package net.jqwik.time.internal.properties.arbitraries;
 
 import java.time.*;
+import java.time.temporal.*;
 
 import org.apiguardian.api.*;
 
@@ -27,4 +28,101 @@ public class DefaultInstantArbitrary extends ArbitraryDecorator<Instant> impleme
 
 	}
 
+	private LocalDateTime instantToLocalDateTime(Instant instant) {
+		if (instant.isAfter(LocalDateTime.MAX.toInstant(ZoneOffset.UTC))) {
+			throw new IllegalArgumentException("Maximum supported year is 999999999 (Year.MAX_VALUE).");
+		}
+		return LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+	}
+
+	@Override
+	public InstantArbitrary atTheEarliest(Instant min) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.atTheEarliest(instantToLocalDateTime(min));
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary atTheLatest(Instant max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.atTheLatest(instantToLocalDateTime(max));
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary dateBetween(LocalDate min, LocalDate max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.dateBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary yearBetween(Year min, Year max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.yearBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary monthBetween(Month min, Month max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.monthBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary onlyMonths(Month... months) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.onlyMonths(months);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary dayOfMonthBetween(int min, int max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.dayOfMonthBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary onlyDaysOfWeek(DayOfWeek... daysOfWeek) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.onlyDaysOfWeek(daysOfWeek);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary timeBetween(LocalTime min, LocalTime max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.timeBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary hourBetween(int min, int max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.hourBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary minuteBetween(int min, int max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.minuteBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary secondBetween(int min, int max) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.secondBetween(min, max);
+		return clone;
+	}
+
+	@Override
+	public InstantArbitrary ofPrecision(ChronoUnit ofPrecision) {
+		DefaultInstantArbitrary clone = typedClone();
+		clone.dateTimeArbitrary = clone.dateTimeArbitrary.ofPrecision(ofPrecision);
+		return clone;
+	}
 }
