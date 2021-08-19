8e39eb4ec197be80bd74a4f9283ef75d64a7f87e
Johannes Link <business@johanneslink.net>
2021-08-19 16:41:31 +0200

    Added FootnotesExamples

---
+++ documentation/src/test/java/net/jqwik/docs/footnotes/FootnotesExamples.java
@@ -0,0 +1,19 @@
+package net.jqwik.docs.footnotes;
+
+import org.assertj.core.api.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.footnotes.*;
+
+@EnableFootnotes
+public class FootnotesExamples {
+
+
+	@Property
+	void differenceShouldBeBelow42(@ForAll int number1, @ForAll int number2, Footnotes footnotes) {
+		int difference = Math.abs(number1 - number2);
+		footnotes.addFootnote(Integer.toString(difference));
+		Assertions.assertThat(difference).isLessThan(42);
+	}
+
+}
