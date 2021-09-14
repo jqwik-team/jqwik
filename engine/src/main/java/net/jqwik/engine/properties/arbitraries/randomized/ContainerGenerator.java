44f5a3c466a9254be725a94980642aa18ded59e0
Dirk Toewe <DirkToewe@GoogleMail.com>
2021-09-14 10:07:10 +0200

    ContainerGenerator Perf. Improv. V2

--- engine/src/main/java/net/jqwik/engine/properties/arbitraries/randomized/ContainerGenerator.java
+++ engine/src/main/java/net/jqwik/engine/properties/arbitraries/randomized/ContainerGenerator.java
@@ -59,7 +59,7 @@ class ContainerGenerator<T, C> implements RandomGenerator<C> {
 								   && uniquenessExtractors.isEmpty()
 								   && random.nextInt(100) <= 2;
 
-		Collection<T> existingValues = noDuplicates ? new HashSet<>() : new ArrayList<>();
+		Collection<T> existingValues = uniquenessExtractors.isEmpty() ? new HashSet<>() : new ArrayList<>();
 
 		while (listOfShrinkables.size() < listSize) {
 			try {
