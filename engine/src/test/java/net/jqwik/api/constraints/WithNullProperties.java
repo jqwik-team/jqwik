127310e54a2973648d7ba01eeaf2045b2338bd66
Johannes Link <business@johanneslink.net>
2021-09-27 15:49:41 +0200

    Fixed Gradle build error

--- engine/src/test/java/net/jqwik/api/constraints/WithNullProperties.java
+++ engine/src/test/java/net/jqwik/api/constraints/WithNullProperties.java
@@ -1,6 +1,5 @@
 package net.jqwik.api.constraints;
 
-import javax.annotation.*;
 import java.util.*;
 import java.util.stream.*;
 
@@ -10,39 +9,39 @@ import net.jqwik.api.statistics.*;
 class WithNullProperties {
 
 	@Property
-	boolean strings(@ForAll @WithNull(1.0) @Nullable String aValue) {
+	boolean strings(@ForAll @WithNull(1.0) String aValue) {
 		return aValue == null;
 	}
 
 	@Property
-	void defaultProbability(@ForAll @WithNull @Nullable String stringOrNull) {
+	void defaultProbability(@ForAll @WithNull String stringOrNull) {
 		Statistics.label("is null")
 				  .collect(stringOrNull == null)
 				  .coverage(coverage -> coverage.check(true).percentage(p -> p >= 1 && p <= 10));
 	}
 
 	@Property
-	boolean lists(@ForAll @WithNull(1.0) @Nullable List<String> aValue) {
+	boolean lists(@ForAll @WithNull(1.0) List<String> aValue) {
 		return aValue == null;
 	}
 
 	@Property
-	boolean sets(@ForAll @WithNull(1.0) @Nullable Set<String> aValue) {
+	boolean sets(@ForAll @WithNull(1.0) Set<String> aValue) {
 		return aValue == null;
 	}
 
 	@Property
-	boolean stream(@ForAll @WithNull(1.0) @Nullable Stream<String> aValue) {
+	boolean stream(@ForAll @WithNull(1.0) Stream<String> aValue) {
 		return aValue == null;
 	}
 
 	@Property
-	boolean arrays(@ForAll @WithNull(1.0) @Nullable String[] aValue) {
+	boolean arrays(@ForAll @WithNull(1.0) String[] aValue) {
 		return aValue == null;
 	}
 
 	@Property
-	boolean providedArbitrary(@ForAll("provided") @WithNull(1.0) @Nullable String aValue) {
+	boolean providedArbitrary(@ForAll("provided") @WithNull(1.0) String aValue) {
 		return aValue == null;
 	}
 
