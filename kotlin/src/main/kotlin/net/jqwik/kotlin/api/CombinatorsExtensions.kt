0bac0adb149e36201060840525ac7b0c36cd1898
Johannes Link <business@johanneslink.net>
2021-10-01 17:34:57 +0200

    Update to Kotlin 1.5.31 to fix compile errors with annotations

--- kotlin/src/main/kotlin/net/jqwik/kotlin/api/CombinatorsExtensions.kt
+++ kotlin/src/main/kotlin/net/jqwik/kotlin/api/CombinatorsExtensions.kt
@@ -82,5 +82,8 @@ fun <T, R> combine(
     Combinators.combine(arbitraries).`as`(f)
 
 
-fun <T1, T2, R> combineFlat(a1: Arbitrary<T1>, a2: Arbitrary<T2>, f: (v1: T1, v2: T2) -> Arbitrary<R>): Arbitrary<R> =
+fun <T1, T2, R> combineFlat(
+    a1: Arbitrary<T1>,
+    a2: Arbitrary<T2>, f: (v1: T1, v2: T2) -> Arbitrary<R>
+): Arbitrary<R> =
     Combinators.combine(a1, a2).flatAs(f)
