aff86db0b908dd2c50d980500de96ccf1cfd5f6b
Johannes Link <business@johanneslink.net>
2021-10-12 11:28:58 +0200

    Kotlin: Made NullabilityTests more robust

--- kotlin/src/test/kotlin/net/jqwik/kotlin/api/NullabilityTests.kt
+++ kotlin/src/test/kotlin/net/jqwik/kotlin/api/NullabilityTests.kt
@@ -27,14 +27,14 @@ class NullabilityTests {
         ) { s: String? -> s == null }
     }
 
-    @Property(edgeCases = EdgeCasesMode.NONE)
+    @Property(tries = 2000, edgeCases = EdgeCasesMode.NONE)
     fun nullableTypesGetNullInjected(@ForAll aNullableString: String?) {
         Statistics.label("aNullableString is null")
             .collect(aNullableString == null)
             .coverage { coverage -> coverage.check(true).percentage(Predicate { p -> p > 3 }) }
     }
 
-    @Property(edgeCases = EdgeCasesMode.NONE)
+    @Property(tries = 2000, edgeCases = EdgeCasesMode.NONE)
     fun nullableTypesGetNullInjectedWithCustomProbability(@ForAll @WithNull(0.5) aNullableString: String?) {
         Statistics.label("aNullableString is null")
             .collect(aNullableString == null)
