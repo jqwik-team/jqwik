9c1c7ae0c76a2c444933505554abcd61c32dc2d3
zinki97 <admin@johannes-games.de>
2021-10-29 00:10:05 +0200

    feedback: ordered ZoneIds with refTime

--- time/src/test/java/net/jqwik/time/api/dateTimes/zonedDateTime/ShrinkingTests.java
+++ time/src/test/java/net/jqwik/time/api/dateTimes/zonedDateTime/ShrinkingTests.java
@@ -16,25 +16,19 @@ import static net.jqwik.testing.ShrinkingSupport.*;
 public class ShrinkingTests {
 
 	@Property
-		//@Disabled("Not working at the moment")
-		//TODO
 	void defaultShrinking(@ForAll Random random) {
 		ZonedDateTimeArbitrary dateTimes = DateTimes.zonedDateTimes();
 		ZonedDateTime value = falsifyThenShrink(dateTimes, random);
-		//assertThat(value).isEqualTo(ZonedDateTime.of(1900, 1, 1, 0, 0, 0, 0, DefaultZonedDateTimeArbitrary.ZONE_ID_IDL));
-		//assertThat(value).isEqualTo(ZonedDateTime.of(1900, 1, 1, 0, 0, 0, 0, ZoneId.of("Pacific/Fiji")));
-		assertThat(value.toLocalDateTime()).isEqualTo(LocalDateTime.of(1900, 1, 1, 0, 0));
+		assertThat(value).isEqualTo(ZonedDateTime.of(1900, 1, 1, 0, 0, 0, 0, ZoneId.of("Etc/GMT-14")));
 	}
 
 	@Property(tries = 40)
-	@Disabled("Not working at the moment")
-		//TODO
 	void shrinksToSmallestFailingValue(@ForAll Random random) {
 		ZonedDateTimeArbitrary dateTimes = DateTimes.zonedDateTimes();
 		TestingFalsifier<ZonedDateTime> falsifier =
 			dateTime -> dateTime.isBefore(ZonedDateTime.of(LocalDateTime.of(2013, MAY, 25, 13, 12, 55), ZoneId.of("Indian/Reunion")));
 		ZonedDateTime value = falsifyThenShrink(dateTimes, random, falsifier);
-		assertThat(value).isEqualTo(ZonedDateTime.of(LocalDateTime.of(2013, MAY, 25, 13, 12, 55), ZoneId.of("Asia/Aden")));
+		assertThat(value).isEqualTo(ZonedDateTime.of(LocalDateTime.of(2013, MAY, 25, 13, 12, 55), ZoneId.of("Africa/Addis_Ababa")));
 	}
 
 }
