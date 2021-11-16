a78d4cf5aaba510ac933d1781bd7132fd2cb5f9a
Johannes Link <business@johanneslink.net>
2021-11-16 09:03:56 +0100

    Added tests for repeatable annotations to JqwikAnnotationSupportTests

--- engine/src/test/java/net/jqwik/engine/support/JqwikAnnotationSupportTests.java
+++ engine/src/test/java/net/jqwik/engine/support/JqwikAnnotationSupportTests.java
@@ -17,25 +17,25 @@ class JqwikAnnotationSupportTests {
 	class FindAllAnnotations {
 		@Example
 		void singlePresentAnnotationIsFound() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("singleAnnotation");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("singleAnnotation");
 			assertThat(types).containsExactly(ForAll.class);
 		}
 
 		@Example
 		void twoAnnotationsAreFound() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("twoAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("twoAnnotations");
 			assertThat(types).containsExactly(ForAll.class, StringLength.class);
 		}
 
 		@Example
 		void repeatableAnnotationsAreFound() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("repeatableAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("repeatableAnnotations");
 			assertThat(types).containsExactly(CharsList.class);
 		}
 
 		@Example
 		void mixedAnnotationsAreFound() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("mixedAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("mixedAnnotations");
 			assertThat(types).containsExactly(ForAll.class, CharsList.class);
 		}
 	}
@@ -44,13 +44,13 @@ class JqwikAnnotationSupportTests {
 	class AllMetaAnnotations {
 		@Example
 		void simpleMetaAnnotation() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("simpleMetaAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("simpleMetaAnnotations");
 			assertThat(types).containsExactly(ForAny.class, ForAll.class);
 		}
 
 		@Example
 		void repeatedMetaAnnotations() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("repeatedMetaAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("repeatedMetaAnnotations");
 			assertThat(types).containsExactly(
 				AlphaChars.class,
 				UpperChars.class, CharRange.class,
@@ -60,7 +60,7 @@ class JqwikAnnotationSupportTests {
 
 		@Example
 		void allMetaAnnotations() throws NoSuchMethodException {
-			Parameter param = parameterFromMethod("repeatedMetaAnnotations");
+			Parameter param = firstParameterFromMethod("repeatedMetaAnnotations");
 			AlphaChars alphaChars = param.getDeclaredAnnotation(AlphaChars.class);
 			List<Annotation> annotations = JqwikAnnotationSupport.allMetaAnnotations(alphaChars);
 			Stream<Class<? extends Annotation>> types = annotations.stream().map(Annotation::annotationType);
@@ -72,33 +72,49 @@ class JqwikAnnotationSupportTests {
 
 		@Example
 		void nestedMetaAnnotations() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("nestedMetaAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("nestedMetaAnnotations");
 			assertThat(types).containsExactly(Nested.class, AlphaNumeric.class, CharsList.class, StringLength.class);
 		}
 
 		@Example
 		void handleCircularMetaAnnotations() throws NoSuchMethodException {
-			Stream<Class<? extends Annotation>> types = typesFromMethod("circularMetaAnnotations");
+			Stream<Class<? extends Annotation>> types = annotationTypesFromMethod("circularMetaAnnotations");
 			assertThat(types).containsExactly(One.class, Two.class);
 		}
 
 	}
 
+	@Group
+	class FindRepeatableAnnotations {
+
+		@Example
+		void repeatableAnnotationsAtMethodAndClass() throws NoSuchMethodException {
+			Method method = AClass.class.getDeclaredMethod("repeatableAnnotations");
+			List<MyRepeatable> annotations = JqwikAnnotationSupport.findRepeatableAnnotationOnElementOrContainer(method, MyRepeatable.class);
+			assertThat(annotations).hasSize(4);
+
+			List<String> annotationValues = annotations.stream().map(a -> a.value()).collect(Collectors.toList());
+			assertThat(annotationValues).containsExactly("atMethod1", "atMethod2", "atClass1", "atClass2");
+		}
+	}
+
 	private List<Annotation> annotationsFromMethod(String methodName) throws NoSuchMethodException {
-		Parameter param = parameterFromMethod(methodName);
+		Parameter param = firstParameterFromMethod(methodName);
 		return JqwikAnnotationSupport.findAllAnnotations(param);
 	}
 
-	private Parameter parameterFromMethod(String methodName) throws NoSuchMethodException {
+	private Parameter firstParameterFromMethod(String methodName) throws NoSuchMethodException {
 		return AClass.class.getDeclaredMethod(methodName, String.class).getParameters()[0];
 	}
 
-	private Stream<Class<? extends Annotation>> typesFromMethod(String methodName) throws NoSuchMethodException {
+	private Stream<Class<? extends Annotation>> annotationTypesFromMethod(String methodName) throws NoSuchMethodException {
 		List<Annotation> annotations = annotationsFromMethod(methodName);
 		return annotations.stream().map(Annotation::annotationType);
 	}
 
-	static class AClass {
+	@MyRepeatable("atClass1")
+	@MyRepeatable("atClass2")
+	private static class AClass {
 		void singleAnnotation(@ForAll String param) {
 		}
 
@@ -121,6 +137,11 @@ class JqwikAnnotationSupportTests {
 		}
 
 		void circularMetaAnnotations(@One String param) {
+		}
+
+		@MyRepeatable("atMethod1")
+		@MyRepeatable("atMethod2")
+		void repeatableAnnotations() {
 		}
 	}
 
@@ -154,6 +175,19 @@ class JqwikAnnotationSupportTests {
 	@Retention(RetentionPolicy.RUNTIME)
 	@One
 	@interface Two {
+	}
+
+	@Target({ ElementType.TYPE, ElementType.METHOD })
+	@Retention(RetentionPolicy.RUNTIME)
+	@Repeatable(MyRepeatableList.class)
+	@interface MyRepeatable {
+		String value();
+	}
+
+	@Target({ ElementType.TYPE, ElementType.METHOD })
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface MyRepeatableList {
+		MyRepeatable[] value();
 	}
 
 }
