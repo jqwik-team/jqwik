58b11302b6b4ad8f6e9e6331395d0d8f05c1088c
Johannes Link <business@johanneslink.net>
2021-11-17 11:28:03 +0100

    Upgrade to Gradle 7.3

--- kotlin/src/main/kotlin/net/jqwik/kotlin/api/BuildersExtensions.kt
+++ kotlin/src/main/kotlin/net/jqwik/kotlin/api/BuildersExtensions.kt
@@ -3,11 +3,15 @@ package net.jqwik.kotlin.api
 import net.jqwik.api.Arbitrary
 import net.jqwik.api.Builders.BuilderCombinator
 import org.apiguardian.api.API
+import java.util.function.BiFunction
 
 /**
  * Convenience function for Kotlin to not use backticked `in` function.
  */
 @API(status = API.Status.EXPERIMENTAL, since = "1.6.0")
 fun <B, T> BuilderCombinator<B>.use(arbitrary: Arbitrary<T>, combinator: (B, T) -> B): BuilderCombinator<B> {
-    return this.use(arbitrary).`in`(combinator)
+    // This explicit conversion form combinator to toFunction is necessary
+    // since the implicit conversion (just handing in combinator) creates nullability warning
+    val toFunction: BiFunction<B, T, B> = BiFunction<B, T, B> { b, t -> combinator(b, t) }
+    return this.use(arbitrary).`in`(toFunction)
 }
