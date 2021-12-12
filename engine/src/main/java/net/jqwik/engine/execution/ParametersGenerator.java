d68a9ec7f44c6af37cb0bf692d6fb0e0e36a12c6
Johannes Link <business@johanneslink.net>
2021-12-12 10:28:12 +0100

    Simplify ParametersGenerator.next signature

--- engine/src/main/java/net/jqwik/engine/execution/ParametersGenerator.java
+++ engine/src/main/java/net/jqwik/engine/execution/ParametersGenerator.java
@@ -1,17 +1,15 @@
 package net.jqwik.engine.execution;
 
 import java.util.*;
-import java.util.function.*;
 
 import net.jqwik.api.*;
-import net.jqwik.api.Tuple.*;
 import net.jqwik.api.lifecycle.*;
 
 public interface ParametersGenerator {
 
 	boolean hasNext();
 
-	Tuple2<TryLifecycleContext, List<Shrinkable<Object>>> next(Supplier<TryLifecycleContext> contextSupplier);
+	List<Shrinkable<Object>> next(TryLifecycleContext context);
 
 	int edgeCasesTotal();
 
