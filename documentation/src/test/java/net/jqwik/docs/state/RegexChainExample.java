4427c623a0fa4d2a2b16d92d397629315f9c48bc
Johannes Link <business@johanneslink.net>
2022-04-01 14:59:41 +0200

    Finished (more or less) RegexChainExample

--- documentation/src/test/java/net/jqwik/docs/state/RegexChainExample.java
+++ documentation/src/test/java/net/jqwik/docs/state/RegexChainExample.java
@@ -1,21 +1,22 @@
 package net.jqwik.docs.state;
 
-import org.assertj.core.api.*;
-
 import net.jqwik.api.*;
 import net.jqwik.api.state.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 import static net.jqwik.api.Arbitraries.*;
 
 class RegexChainExample {
 
-	@Property
+	@Property(tries = 10)
 	void generateABPlusC(@ForAll("abplusc") Chain<String> regex) {
 		String string = null;
-		for (String value: regex) {
+		for (String value : regex) {
 			string = value;
 		}
-		Assertions.assertThat(string).matches("ab+c");
+		System.out.println(string);
+		assertThat(string).matches("ab+c");
 	}
 
 	@Provide
@@ -24,12 +25,21 @@ class RegexChainExample {
 			() -> "",
 			stringSupplier -> {
 				String value = stringSupplier.get();
-				if (true) { //value.isEmpty()) {
+				if (value.isEmpty()) {
 					return just(s -> s + "a");
+				}
+				if (value.endsWith("a")) {
+					return just(s -> s + "b");
+				}
+				if (value.endsWith("b")) {
+					return frequency(
+						Tuple.of(5, s -> s + "b"),
+						Tuple.of(1, s -> s + "c")
+					);
 				}
 				return just(Transformer.endOfChain());
 			}
-		);
+		).infinite().dontShrink();
 	}
 
 }
