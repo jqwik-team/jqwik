33d4185752397e3fefc27f45a0b0f040145cd010
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2022-05-26 10:01:34 +0300

    Avoid recursion in CombinedIterator
    Previously, CombinedIterator#next copied the list as many times
    as there were elements in the list (each recursive call did copy the list).
    
    Now only a single copy is made.
    
    It improves performance, and it makes stacktraces easier to understand.
    
    fixes #342

--- engine/src/main/java/net/jqwik/engine/support/combinatorics/CombinedIterator.java
+++ engine/src/main/java/net/jqwik/engine/support/combinatorics/CombinedIterator.java
@@ -2,62 +2,73 @@ package net.jqwik.engine.support.combinatorics;
 
 import java.util.ArrayList;
 import java.util.*;
-
-import static java.util.Arrays.*;
+import java.util.stream.*;
 
 public class CombinedIterator<T> implements Iterator<List<T>> {
-
-	private final Iterator<T> first;
-	private final ArrayList<Iterable<T>> rest;
-	private Iterator<List<T>> next;
-
-	private T current = null;
-
-	// This must be tracked because there can be null values
-	private boolean currentIsSet = false;
+	private final List<Iterable<T>> iterables;
+	private final List<Iterator<T>> iterators;
+	private final List<T> elements;
+	private final boolean isEmpty;
+	private int position = -1;
 
 	public CombinedIterator(List<Iterable<T>> iterables) {
-		this.rest = new ArrayList<>(iterables);
-		this.first = this.rest.remove(0).iterator();
-		this.next = restIterator();
-	}
-
-	private Iterator<List<T>> restIterator() {
-		return this.rest.isEmpty()
-				   ? emptyListIterator()
-				   : new CombinedIterator<>(this.rest);
-	}
-
-	private Iterator<List<T>> emptyListIterator() {
-		return asList(Collections.<T>emptyList()).iterator();
+		this.iterables = iterables;
+		elements = new ArrayList<>(Collections.nCopies(iterables.size(), null));
+		iterators = iterables.stream().map(Iterable::iterator).collect(Collectors.toCollection(ArrayList::new));
+		isEmpty = !iterators.stream().allMatch(Iterator::hasNext);
 	}
 
 	@Override
 	public boolean hasNext() {
-		if (currentIsSet) {
-			return next.hasNext() || first.hasNext();
-		} else {
-			return next.hasNext() && first.hasNext();
+		if (isEmpty) {
+			return false;
 		}
+		return position == -1 || nextAvailablePosition() != -1;
 	}
 
 	@Override
 	public List<T> next() {
-		if (next.hasNext()) {
-			if (!currentIsSet) {
-				current = first.next();
-				currentIsSet = true;
-			}
-		} else {
-			current = first.next();
-			this.next = restIterator();
+		if (isEmpty) {
+			throw new NoSuchElementException();
 		}
-		return prepend(current, next.next());
+		if (position == -1) {
+			// The first initialization of the values
+			resetValuesUpTo(elements.size());
+		} else {
+			Iterator<T> it = iterators.get(position);
+			if (it.hasNext()) {
+				// Just advance the current iterator
+				elements.set(position, it.next());
+			} else {
+				// Advance the next iterator, and reset [0..nextPosition)
+				position++;
+				int nextPosition = nextAvailablePosition();
+				if (nextPosition == -1) {
+					throw new NoSuchElementException();
+				}
+				elements.set(nextPosition, iterators.get(nextPosition).next());
+				resetValuesUpTo(nextPosition);
+			}
+		}
+		return new ArrayList<>(elements);
 	}
 
-	private List<T> prepend(T head, List<T> tail) {
-		List<T> rest = new ArrayList<>(tail);
-		rest.add(0, head);
-		return rest;
+	private void resetValuesUpTo(int nextPosition) {
+		for (int i = 0; i < nextPosition; i++) {
+			Iterator<T> newIt = iterables.get(i).iterator();
+			iterators.set(i, newIt);
+			elements.set(i, newIt.next());
+		}
+		position = 0;
+	}
+
+	private int nextAvailablePosition() {
+		int size = iterators.size();
+		for (int i = position; i < size; i++) {
+			if (iterators.get(i).hasNext()) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
