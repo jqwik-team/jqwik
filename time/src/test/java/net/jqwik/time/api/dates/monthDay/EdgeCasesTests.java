e9e5ccb2761976e6c6123573ea8ea39ec42460ed
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2022-05-28 15:56:33 +0300

    Correct expected output in net.jqwik.time.api.dates.monthDay.EdgeCasesTests#all

--- time/src/test/java/net/jqwik/time/api/dates/monthDay/EdgeCasesTests.java
+++ time/src/test/java/net/jqwik/time/api/dates/monthDay/EdgeCasesTests.java
@@ -18,10 +18,13 @@ public class EdgeCasesTests {
 		MonthDayArbitrary monthDays = Dates.monthDays();
 		Set<MonthDay> edgeCases = collectEdgeCaseValues(monthDays.edgeCases());
 		assertThat(edgeCases).hasSize(3);
+		// February 29 is added via edgeCases.add in net.jqwik.time.internal.properties.arbitraries.DefaultLocalDateArbitrary.arbitrary
+		// And net.jqwik.engine.properties.arbitraries.GenericEdgeCasesConfiguration.configure adds all the explicitly
+		// added edge cases to the end of the list. So February 29 is generated the last.
 		assertThat(edgeCases).containsExactly(
 			MonthDay.of(Month.JANUARY, 1),
-			MonthDay.of(Month.FEBRUARY, 29),
-			MonthDay.of(Month.DECEMBER, 31)
+			MonthDay.of(Month.DECEMBER, 31),
+			MonthDay.of(Month.FEBRUARY, 29)
 		);
 	}
 
