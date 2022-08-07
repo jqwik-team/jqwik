a2fadfc83b224623b0838cc0ab0bec91c8ce7a23
Johannes Link <business@johanneslink.net>
2022-08-07 18:25:52 +0200

    Moved rendered docs to github/jlink/jqwik.net

--- docs/docs/1.3.8/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.jqwik.api"},{"l":"net.jqwik.api.arbitraries"},{"l":"net.jqwik.api.configurators"},{"l":"net.jqwik.api.constraints"},{"l":"net.jqwik.api.domains"},{"l":"net.jqwik.api.lifecycle"},{"l":"net.jqwik.api.providers"},{"l":"net.jqwik.api.stateful"},{"l":"net.jqwik.api.statistics"}]
