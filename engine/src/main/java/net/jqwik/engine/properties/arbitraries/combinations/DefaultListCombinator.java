b5d4bd46da3e9f17ba1e7feb906ba33b1e1a581b
Johannes Link <business@johanneslink.net>
2022-10-10 13:21:03 +0200

    Refactored DefaultListCombinator

--- engine/src/main/java/net/jqwik/engine/properties/arbitraries/combinations/DefaultListCombinator.java
+++ engine/src/main/java/net/jqwik/engine/properties/arbitraries/combinations/DefaultListCombinator.java
@@ -12,22 +12,56 @@ import static org.apiguardian.api.API.Status.*;
 
 public class DefaultListCombinator<T> implements Combinators.ListCombinator<T> {
 
-	private final Arbitrary<T>[] arbitraries;
-
-	public DefaultListCombinator(List<Arbitrary<T>> listOfArbitraries) {
-		arbitraries = listOfArbitraries.toArray(new Arbitrary[listOfArbitraries.size()]);
-	}
+	protected final Arbitrary<T>[] arbitraries;
 
 	@SuppressWarnings("unchecked")
+	public DefaultListCombinator(List<Arbitrary<T>> listOfArbitraries) {
+		this(listOfArbitraries.toArray(new Arbitrary[0]));
+	}
+
+	private DefaultListCombinator(Arbitrary<T>[] arbitraries) {
+		this.arbitraries = arbitraries;
+	}
+
 	@Override
 	public <R> Arbitrary<R> as(Function<List<T>, @NotNull R> combinator) {
-		final Function<List<Object>, R> combineFunction = params -> combinator.apply((List<T>) params);
-		return new CombineArbitrary<>(combineFunction, arbitraries);
+		return new CombineArbitrary<>(combineFunction(combinator), arbitraries);
 	}
 
 	@API(status = EXPERIMENTAL, since = "1.7.1")
 	public Combinators.ListCombinator<T> filter(Predicate<List<T>> filter) {
-		return new FilteredListCombinator<>(arbitraries, filter);
+		return new Filtered<>(arbitraries, filter);
 	}
 
+	@SuppressWarnings("unchecked")
+	protected <R> Function<List<Object>, R> combineFunction(Function<List<T>, R> combinator) {
+		return params -> combinator.apply((List<T>) params);
+	}
+
+	private static class Filtered<T> extends DefaultListCombinator<T> {
+		private final Predicate<List<T>> filter;
+
+		private Filtered(Arbitrary<T>[] arbitraries, Predicate<List<T>> filter) {
+			super(arbitraries);
+			this.filter = filter;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public <R> Arbitrary<R> as(Function<List<T>, @NotNull R> combinator) {
+			Predicate<List<Object>> filterPredicate = params -> filter.test((List<T>) params);
+			return new CombineArbitrary<>(Function.identity(), arbitraries)
+					   .filter(filterPredicate)
+					   .map(combineFunction(combinator));
+		}
+
+		@Override
+		public Combinators.ListCombinator<T> filter(Predicate<List<T>> filter) {
+			return super.filter(combineFilters(this.filter, filter));
+		}
+
+		private Predicate<List<T>> combineFilters(Predicate<List<T>> first, Predicate<List<T>> second) {
+			return first.and(second);
+		}
+	}
 }
