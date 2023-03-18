32c8f268efed346bb9172d2b55d7b998ac973058
Johannes Link <business@johanneslink.net>
2023-03-18 15:09:06 +0100

    Added user guide section on BeforeTry member variables

--- engine/src/test/java/net/jqwik/engine/execution/lifecycle/BeforeTryOnMemberVariableTests.java
+++ engine/src/test/java/net/jqwik/engine/execution/lifecycle/BeforeTryOnMemberVariableTests.java
@@ -11,13 +11,26 @@ class BeforeTryOnMemberVariableTests {
 	//@BeforeTry // will lead to exception since static fields must not use @BeforeTry
 	private static final int staticMember = 40;
 
+	private BeforeTryOnMemberVariableTests() {
+		memberInitializedInConstructor = 99;
+	}
+
 	@BeforeTry
 	private int member = 41;
 
+	@BeforeTry
+	private int memberInitializedInConstructor;
+
 	@Property(tries = 10)
-	void beforeTryOnMember(@ForAll @IntRange(min = 1, max = 100) int addend) {
+	void memberInitializedInDeclaration(@ForAll @IntRange(min = 1, max = 100) int addend) {
 		Assertions.assertThat(member).isEqualTo(41);
 		member = member + addend;
+	}
+
+	@Property(tries = 10)
+	void memberInitializedInConstructor(@ForAll @IntRange(min = 1, max = 100) int addend) {
+		Assertions.assertThat(memberInitializedInConstructor).isEqualTo(99);
+		memberInitializedInConstructor = memberInitializedInConstructor + addend;
 	}
 
 	@Group
