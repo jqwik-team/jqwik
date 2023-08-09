ac238df3206001c9298cab14f5ba28ab0abbf71c
Johannes Link <business@johanneslink.net>
2023-08-09 13:47:05 +0200

    ParameterAnnotationEnhancer is now only applied to parameters within Kotlin files.

--- kotlin/src/main/kotlin/net/jqwik/kotlin/internal/ParameterAnnotationEnhancer.kt
+++ kotlin/src/main/kotlin/net/jqwik/kotlin/internal/ParameterAnnotationEnhancer.kt
@@ -2,11 +2,15 @@ package net.jqwik.kotlin.internal
 
 import net.jqwik.api.providers.TypeUsage
 import java.lang.reflect.Parameter
-import kotlin.reflect.KParameter
 
+/**
+ * Annotations of a Kotlin's parameter's type (e.g. aParam: @MyAnnotation MyType) must be added explicitly.
+ */
 class ParameterAnnotationEnhancer : TypeUsage.Enhancer {
 
     override fun forParameter(original: TypeUsage, parameter: Parameter): TypeUsage {
+        val parameterDeclaredInKotlinClass = parameter.declaringExecutable.declaringClass.isKotlinClass()
+        if (!parameterDeclaredInKotlinClass) return original
         val typeAnnotations = parameter.annotatedType.annotations
         if (typeAnnotations.isEmpty()) {
             return original
