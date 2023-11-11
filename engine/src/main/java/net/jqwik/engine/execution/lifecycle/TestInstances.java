76affc2210e952fd625ded7e1ec09ff5db049359
Johannes Link <business@johanneslink.net>
2023-11-11 11:43:03 +0100

    Add type for holding test instances

---
+++ engine/src/main/java/net/jqwik/engine/execution/lifecycle/TestInstances.java
@@ -0,0 +1,45 @@
+package net.jqwik.engine.execution.lifecycle;
+
+import java.util.*;
+
+/**
+ * Wraps the instance of a test container class and potentially its outer classes.
+ */
+public class TestInstances {
+
+	private final List<Object> testInstances;
+
+	public TestInstances(Object testInstance) {
+		this.testInstances = Collections.singletonList(testInstance);
+	}
+
+	public TestInstances(Object testInstance, TestInstances outerInstances) {
+		this.testInstances = new ArrayList<>(outerInstances.testInstances);
+		this.testInstances.addFirst(testInstance);
+	}
+
+	private TestInstances(List<Object> testInstances) {
+		this.testInstances = testInstances;
+	}
+
+	public Object target() {
+		if (testInstances.size() > 1) {
+			return testInstances.get(1);
+		} else {
+			throw new IllegalStateException("No target instance available");
+		}
+	}
+
+	public Optional<TestInstances> outer() {
+		if (testInstances.size() > 2) {
+			List<Object> rest = testInstances.subList(1, testInstances.size());
+			return Optional.of(new TestInstances(rest));
+		} else {
+			return Optional.empty();
+		}
+	}
+
+	public List<Object> all() {
+		return testInstances;
+	}
+}
