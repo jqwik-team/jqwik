4c0da39eb25a5e6fe78dfaac2ac6322fc8977963
Jibidus <jibidus@gmail.com>
2024-02-24 08:47:09 +0100

    Added anyForSubtypeOf() with recusrion of sealed subclasses

--- kotlin/src/test/kotlin/net/jqwik/kotlin/AnyForSubtypeTests.kt
+++ kotlin/src/test/kotlin/net/jqwik/kotlin/AnyForSubtypeTests.kt
@@ -2,8 +2,11 @@ package net.jqwik.kotlin
 
 import net.jqwik.api.Example
 import net.jqwik.api.ForAll
+import net.jqwik.api.Property
+import net.jqwik.api.Provide
 import net.jqwik.kotlin.api.anyForSubtypeOf
 import net.jqwik.testing.TestingSupport
+import org.assertj.core.api.Assertions.assertThat
 import java.util.*
 
 class AnyForSubtypeTests {
@@ -34,5 +37,23 @@ class AnyForSubtypeTests {
     fun `anyForSubtypeOf() with arbitrary recursion`(@ForAll random: Random) {
         val subtypes = anyForSubtypeOf<ParentWithRecursion>(enableArbitraryRecursion = true)
         TestingSupport.checkAllGenerated(subtypes, random) { it is ChildWithCustomType }
+    }
+
+    sealed interface ParentInterface
+    sealed interface ChildInterface : ParentInterface
+    sealed class ChildClass : ParentInterface
+    class ChildInterfaceImpl(val value: String) : ChildInterface
+    class ChildClassImpl(val value: String) : ChildClass()
+
+    @Provide
+    fun parentInterface() = anyForSubtypeOf<ParentInterface>()
+
+    @Property
+    fun `anyForSubtypeOf() returns type arbitrary for any concrete subtype of a given sealed class or interface, even nested`(
+        @ForAll(
+            "parentInterface"
+        ) parent: ParentInterface
+    ) {
+        assertThat(parent).matches { it is ChildInterfaceImpl || it is ChildClassImpl }
     }
 }
