48a76f8c9dcc7137645d29cfbaa9f1dd8fb243b8
Johannes Link <business@johanneslink.net>
2025-11-23 13:17:23 +0100

    Fixed Gradle task `publish` to publish all modules and announce publication to central.sonatype.com

--- signalPublishToCentralSonatype.sh
+++ signalPublishToCentralSonatype.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+#set -x
 token=$1
-echo "Posting new deployment to ossrh-staging.api.central"
+echo "Posting new deployment to ossrh-staging-api.central"
 curl -X 'POST' \
 			  'https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/net.jqwik' \
 			  -H 'accept: application/json' \
-			  -H 'Authorization: $token'
+			  -H "Authorization: Bearer $token"
